b356ef812975f24945747d35041268002a95f85a
diekmann <diekmann@net.in.tum.de>
2014-10-16 15:51:46 +0200

    changed the packet set structure again
    packet_set_not_correct

--- Packet_Set.thy
+++ Packet_Set.thy
@@ -48,13 +48,25 @@ fun packet_set_filter :: "('a, 'p) match_tac \<Rightarrow> action \<Rightarrow> 
 
 text{*Symbolic (executable) representation. inner is @{text \<and>}, outer is @{text \<or>}*}
 (*we remember the action which might be necessary for applying \<alpha>*)
-datatype_new 'a packet_set = PacketSet (packet_set_repr: "(('a negation_type \<times> action) list) list")
+(*the @{typ "'a"} negation type tells whether to negate the match expression*)
+(*test: the action negation tells whether to negate the result of the match*)
+(*due to unknowns, this may make a huge difference!*)
+datatype_new 'a packet_set = PacketSet (packet_set_repr: "(('a negation_type \<times> action negation_type) list) list")
 
 definition to_packet_set :: "action \<Rightarrow> 'a match_expr \<Rightarrow> 'a packet_set" where
- "to_packet_set a m = PacketSet (map (map (\<lambda>m'. (m',a)) o to_negation_type_nnf) (normalize_match m))"
+ "to_packet_set a m = PacketSet (map (map (\<lambda>m'. (m',Pos a)) o to_negation_type_nnf) (normalize_match m))"
+
+fun get_action :: "action negation_type \<Rightarrow> action" where
+  "get_action (Pos a) = a" |
+  "get_action (Neg a) = a"
+
+
+fun get_action_sign :: "action negation_type \<Rightarrow> (bool \<Rightarrow> bool)" where
+  "get_action_sign (Pos _) = id" |
+  "get_action_sign (Neg _) = (\<lambda>m. \<not> m)"
 
 definition packet_set_to_set :: "('a, 'packet) match_tac \<Rightarrow> 'a packet_set \<Rightarrow> 'packet set" where
-  "packet_set_to_set \<gamma> ps \<equiv> \<Union> ms \<in> set (packet_set_repr ps).  {p. \<forall> (m, a) \<in> set ms. matches \<gamma> (negation_type_to_match_expr m) a p}"
+  "packet_set_to_set \<gamma> ps \<equiv> \<Union> ms \<in> set (packet_set_repr ps).  {p. \<forall> (m, a) \<in> set ms. get_action_sign a (matches \<gamma> (negation_type_to_match_expr m) (get_action a) p)}"
 
 lemma to_packet_set_correct: "p \<in> packet_set_to_set \<gamma> (to_packet_set a m) \<longleftrightarrow> matches \<gamma> m a p"
 apply(simp add: to_packet_set_def packet_set_to_set_def)
@@ -88,18 +100,25 @@ value "packet_set_and (PacketSet [[a,b], [c,d]]) (PacketSet [[v,w], [x,y]])"
 
 declare packet_set_and.simps[simp del]
 
+
+(*TODO move*)
+lemma packet_set_to_set_alt:  "packet_set_to_set \<gamma> ps = (\<Union> ms \<in> set (packet_set_repr ps).  
+  {p. \<forall> m a. (m, a) \<in> set ms \<longrightarrow> get_action_sign a (matches \<gamma> (negation_type_to_match_expr m) (get_action a) p)})"
+unfolding packet_set_to_set_def
+by fast
+
 lemma packet_set_and_correct: "packet_set_to_set \<gamma> (packet_set_and (to_packet_set a m1) (to_packet_set a m2)) = packet_set_to_set \<gamma> (to_packet_set a (MatchAnd m1 m2))"
- apply(simp add: to_packet_set_def packet_set_and.simps packet_set_to_set_def)
- by fast
+ apply(simp add: to_packet_set_def packet_set_and.simps packet_set_to_set_alt)
+ (*by fast very slow!*)
+ apply safe
+ apply simp_all
+ apply blast+
+ done
  
 lemma packet_set_and_correct': "p \<in> packet_set_to_set \<gamma> (packet_set_and (to_packet_set a m1) (to_packet_set a m2)) \<longleftrightarrow> matches \<gamma> (MatchAnd m1 m2) a p"
 apply(simp add: to_packet_set_correct[symmetric])
 using packet_set_and_correct by fast
 
-(*TODO move*)
-lemma packet_set_to_set_alt:  "packet_set_to_set \<gamma> ps = (\<Union> ms \<in> set (packet_set_repr ps).  {p. \<forall> m a. (m, a) \<in> set ms \<longrightarrow> matches \<gamma> (negation_type_to_match_expr m) a p})"
-unfolding packet_set_to_set_def
-by fast
 
 lemma packet_set_and_union: "packet_set_to_set \<gamma> (packet_set_and P1 P2) = packet_set_to_set \<gamma> P1 \<inter> packet_set_to_set \<gamma> P2"
 unfolding packet_set_to_set_def
@@ -152,48 +171,58 @@ lemma listprepend_correct: "packet_set_to_set \<gamma> (PacketSet (listprepend a
   by blast
 
 (*begin scratch*)
-(*
-fun invertt :: "('a negation_type \<times> action) \<Rightarrow> ('a negation_type \<times> action)" where
-  "invertt (Pos n, a) = (Neg n, a)" |
-  "invertt (Neg n, a) = (Pos n, a)"
 
-lemma "packet_set_to_set \<gamma> (PacketSet [[invertt n]]) = UNIV - packet_set_to_set \<gamma> (PacketSet [[n]])"
-nitpick
-oops
+lemma packet_set_to_set_map_singleton: "packet_set_to_set \<gamma> (PacketSet (map (\<lambda>a. [a]) as)) = (\<Union> a \<in> set as. packet_set_to_set \<gamma> (PacketSet [[a]]))"
+by(simp add: packet_set_to_set_alt)
 
-fun packet_set_not_internal :: " ('a negation_type \<times> action) list list \<Rightarrow>  ('a negation_type \<times> action) list list" where
+fun invertt :: "('a negation_type \<times> action negation_type) \<Rightarrow> ('a negation_type \<times> action negation_type)" where
+  "invertt (n, a) = (n, invert a)"
+
+lemma singleton_invertt: "packet_set_to_set \<gamma> (PacketSet [[invertt n]]) =  - packet_set_to_set \<gamma> (PacketSet [[n]])"
+ apply(simp add: to_packet_set_def packet_set_and.simps packet_set_to_set_alt)
+ apply(case_tac n, rename_tac m a)
+ apply(simp)
+ apply(case_tac a)
+  apply(simp_all)
+  apply safe
+ done
+
+lemma packet_set_to_set_map_singleton_invertt: 
+  "packet_set_to_set \<gamma> (PacketSet (map ((\<lambda>a. [a]) \<circ> invertt) d)) = - (\<Inter> a \<in> set d. packet_set_to_set \<gamma> (PacketSet [[a]]))"
+apply(induction d)
+ apply(simp)
+ apply(simp add: packet_set_to_set_alt)
+apply(simp add: )
+apply(subst(1) packet_set_cons)
+apply(simp)
+apply(simp add: packet_set_to_set_map_singleton singleton_invertt)
+done
+
+fun packet_set_not_internal :: " ('a negation_type \<times> action negation_type) list list \<Rightarrow>  ('a negation_type \<times> action  negation_type) list list" where
   "packet_set_not_internal [] = [[]]" |
-  "packet_set_not_internal (ns#nss) = listprepend (map (\<lambda>(n,a). (invert n,a)) ns) (packet_set_not_internal nss)"
+  "packet_set_not_internal (ns#nss) = listprepend (map invertt ns) (packet_set_not_internal nss)"
 
 
-lemma "packet_set_to_set \<gamma> (PacketSet (packet_set_not_internal d)) = UNIV - packet_set_to_set \<gamma> (PacketSet d)"
-nitpick
-(*unknown inverting is wrong\<And>*)
+lemma packet_set_not_internal_correct: "packet_set_to_set \<gamma> (PacketSet (packet_set_not_internal d)) = - packet_set_to_set \<gamma> (PacketSet d)"
   apply(induction d)
    apply(simp add: packet_set_to_set_alt)
+  apply(rename_tac d ds)
   apply(simp add: )
   apply(simp add: listprepend_correct)
+  apply(simp add: packet_set_to_set_map_singleton_invertt)
   apply(simp add: packet_set_to_set_alt)
-  apply safe
-  apply simp_all
-  
-  apply(simp add: cnf_invert_singelton cnf_singleton_false)
-  done
+  by blast
 
 fun packet_set_not :: "'a packet_set \<Rightarrow> 'a packet_set" where
-  "packet_set_not (PacketSet ps) = PacketSet [map (\<lambda>(n,a). (invert n,a)) ns. ns <- ps]"
+  "packet_set_not (PacketSet ps) = PacketSet (packet_set_not_internal ps)"
 declare packet_set_not.simps[simp del]
 
-lemma "packet_set_to_set \<gamma> (packet_set_not P) = - packet_set_to_set \<gamma> P"
+lemma packet_set_not_correct: "packet_set_to_set \<gamma> (packet_set_not P) = - packet_set_to_set \<gamma> P"
 apply(cases P)
 apply(simp)
 apply(simp add: packet_set_not.simps)
-apply(simp add: packet_set_to_set_alt)
-apply(safe)
-
-oops
-(*end scratch*)
-*)
+apply(simp add: packet_set_not_internal_correct)
+done
 
 
 
