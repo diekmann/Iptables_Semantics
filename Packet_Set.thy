883a59f9e045f7816387459000b83a4951273e2e
diekmann <diekmann@net.in.tum.de>
2014-10-21 12:26:06 +0200

    collect_deny

--- Packet_Set.thy
+++ Packet_Set.thy
@@ -23,96 +23,188 @@ section{*Packet Set*}
 (*probably everything here wants a simple ruleset*)
 
 
+(*TODO: collect_by_action
+has_Default \<Longrightarrow> UNIV - collect_drop = collect_allow
+*)
+
 subsection{*The set of all accepted packets*}
-text{*
-Collect all packets which are allowed by the firewall.
-*}
-fun collect_allow :: "('a, 'p) match_tac \<Rightarrow> 'a rule list \<Rightarrow> 'p set \<Rightarrow> 'p set" where
-  "collect_allow _ [] P = {}" |
-  "collect_allow \<gamma> ((Rule m Accept)#rs) P = {p \<in> P. matches \<gamma> m Accept p} \<union> (collect_allow \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Accept p})" |
-  "collect_allow \<gamma> ((Rule m Drop)#rs) P = (collect_allow \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Drop p})"
-
-lemma collect_allow_subset: "simple_ruleset rs \<Longrightarrow> collect_allow \<gamma> rs P \<subseteq> P"
-apply(induction rs arbitrary: P)
- apply(simp)
-apply(rename_tac r rs P)
-apply(case_tac r, rename_tac m a)
-apply(case_tac a)
-apply(simp_all add: simple_ruleset_def)
-apply(fast)
-apply blast
-done
-
-
-lemma collect_allow_sound: "simple_ruleset rs \<Longrightarrow> p \<in> collect_allow \<gamma> rs P \<Longrightarrow> approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalAllow"
-proof(induction rs arbitrary: P)
-case Nil thus ?case by simp
-next
-case (Cons r rs)
-  from Cons obtain m a where r: "r = Rule m a" by (cases r) simp
-  from Cons.prems have simple_rs: "simple_ruleset rs" by (simp add: r simple_ruleset_def)
-  from Cons.prems r have a_cases: "a = Accept \<or> a = Drop" by (simp add: r simple_ruleset_def)
-  show ?case (is ?goal)
-  proof(cases a)
-    case Accept
-      from Accept Cons.IH[where P="{p \<in> P. \<not> matches \<gamma> m Accept p}"] simple_rs have IH:
-        "p \<in> collect_allow \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Accept p} \<Longrightarrow> approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalAllow" by simp
-      from Accept Cons.prems have "(p \<in> P \<and> matches \<gamma> m Accept p) \<or> p \<in> collect_allow \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Accept p}"
-        by(simp add: r)
-      with Accept show ?goal
-      apply -
-      apply(erule disjE)
-       apply(simp add: r)
-      apply(simp add: r)
-      using IH by blast
-    next
-    case Drop 
-      from Drop Cons.prems have "p \<in> collect_allow \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Drop p}"
-        by(simp add: r)
-      with Cons.IH simple_rs have "approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalAllow" by simp
-      with Cons show ?goal
-       apply(simp add: r Drop del: approximating_bigstep_fun.simps)
-       apply(simp)
-       using collect_allow_subset[OF simple_rs] by fast
-    qed(insert a_cases, simp_all)
-qed
-
-
-lemma collect_allow_complete: "simple_ruleset rs \<Longrightarrow> approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalAllow \<Longrightarrow> p \<in> P \<Longrightarrow> p \<in> collect_allow \<gamma> rs P"
-proof(induction rs arbitrary: P)
-case Nil thus ?case by simp
-next
-case (Cons r rs)
-  from Cons obtain m a where r: "r = Rule m a" by (cases r) simp
-  from Cons.prems have simple_rs: "simple_ruleset rs" by (simp add: r simple_ruleset_def)
-  from Cons.prems r have a_cases: "a = Accept \<or> a = Drop" by (simp add: r simple_ruleset_def)
-  show ?case (is ?goal)
-  proof(cases a)
-    case Accept
-      from Accept Cons.IH simple_rs have IH: "\<forall>P. approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalAllow \<longrightarrow> p \<in> P \<longrightarrow> p \<in> collect_allow \<gamma> rs P" by simp
-      with Accept Cons.prems show ?goal
-        apply(cases "matches \<gamma> m Accept p")
+  text{*
+  Collect all packets which are allowed by the firewall.
+  *}
+  fun collect_allow :: "('a, 'p) match_tac \<Rightarrow> 'a rule list \<Rightarrow> 'p set \<Rightarrow> 'p set" where
+    "collect_allow _ [] P = {}" |
+    "collect_allow \<gamma> ((Rule m Accept)#rs) P = {p \<in> P. matches \<gamma> m Accept p} \<union> (collect_allow \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Accept p})" |
+    "collect_allow \<gamma> ((Rule m Drop)#rs) P = (collect_allow \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Drop p})"
+  
+  lemma collect_allow_subset: "simple_ruleset rs \<Longrightarrow> collect_allow \<gamma> rs P \<subseteq> P"
+  apply(induction rs arbitrary: P)
+   apply(simp)
+  apply(rename_tac r rs P)
+  apply(case_tac r, rename_tac m a)
+  apply(case_tac a)
+  apply(simp_all add: simple_ruleset_def)
+  apply(fast)
+  apply blast
+  done
+  
+  
+  lemma collect_allow_sound: "simple_ruleset rs \<Longrightarrow> p \<in> collect_allow \<gamma> rs P \<Longrightarrow> approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalAllow"
+  proof(induction rs arbitrary: P)
+  case Nil thus ?case by simp
+  next
+  case (Cons r rs)
+    from Cons obtain m a where r: "r = Rule m a" by (cases r) simp
+    from Cons.prems have simple_rs: "simple_ruleset rs" by (simp add: r simple_ruleset_def)
+    from Cons.prems r have a_cases: "a = Accept \<or> a = Drop" by (simp add: r simple_ruleset_def)
+    show ?case (is ?goal)
+    proof(cases a)
+      case Accept
+        from Accept Cons.IH[where P="{p \<in> P. \<not> matches \<gamma> m Accept p}"] simple_rs have IH:
+          "p \<in> collect_allow \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Accept p} \<Longrightarrow> approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalAllow" by simp
+        from Accept Cons.prems have "(p \<in> P \<and> matches \<gamma> m Accept p) \<or> p \<in> collect_allow \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Accept p}"
+          by(simp add: r)
+        with Accept show ?goal
+        apply -
+        apply(erule disjE)
          apply(simp add: r)
         apply(simp add: r)
-        done
-    next
-    case Drop
-      with Cons show ?goal 
-        apply(case_tac "matches \<gamma> m Drop p")
+        using IH by blast
+      next
+      case Drop 
+        from Drop Cons.prems have "p \<in> collect_allow \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Drop p}"
+          by(simp add: r)
+        with Cons.IH simple_rs have "approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalAllow" by simp
+        with Cons show ?goal
+         apply(simp add: r Drop del: approximating_bigstep_fun.simps)
+         apply(simp)
+         using collect_allow_subset[OF simple_rs] by fast
+      qed(insert a_cases, simp_all)
+  qed
+  
+  
+  lemma collect_allow_complete: "simple_ruleset rs \<Longrightarrow> approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalAllow \<Longrightarrow> p \<in> P \<Longrightarrow> p \<in> collect_allow \<gamma> rs P"
+  proof(induction rs arbitrary: P)
+  case Nil thus ?case by simp
+  next
+  case (Cons r rs)
+    from Cons obtain m a where r: "r = Rule m a" by (cases r) simp
+    from Cons.prems have simple_rs: "simple_ruleset rs" by (simp add: r simple_ruleset_def)
+    from Cons.prems r have a_cases: "a = Accept \<or> a = Drop" by (simp add: r simple_ruleset_def)
+    show ?case (is ?goal)
+    proof(cases a)
+      case Accept
+        from Accept Cons.IH simple_rs have IH: "\<forall>P. approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalAllow \<longrightarrow> p \<in> P \<longrightarrow> p \<in> collect_allow \<gamma> rs P" by simp
+        with Accept Cons.prems show ?goal
+          apply(cases "matches \<gamma> m Accept p")
+           apply(simp add: r)
+          apply(simp add: r)
+          done
+      next
+      case Drop
+        with Cons show ?goal 
+          apply(case_tac "matches \<gamma> m Drop p")
+           apply(simp add: r)
+          apply(simp add: r simple_rs)
+          done
+      qed(insert a_cases, simp_all)
+  qed
+  
+  
+  theorem collect_allow_sound_complete: "simple_ruleset rs \<Longrightarrow> {p. p \<in> collect_allow \<gamma> rs UNIV} = {p. approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalAllow}"
+  apply(safe)
+  using collect_allow_sound[where P=UNIV] apply fast
+  using collect_allow_complete[where P=UNIV] by fast
+
+
+
+subsection{*The set of all dropped packets*}
+  text{*
+  Collect all packets which are denied by the firewall.
+  *}
+  fun collect_deny :: "('a, 'p) match_tac \<Rightarrow> 'a rule list \<Rightarrow> 'p set \<Rightarrow> 'p set" where
+    "collect_deny _ [] P = {}" |
+    "collect_deny \<gamma> ((Rule m Drop)#rs) P = {p \<in> P. matches \<gamma> m Drop p} \<union> (collect_deny \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Drop p})" |
+    "collect_deny \<gamma> ((Rule m Accept)#rs) P = (collect_deny \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Accept p})"
+  
+  lemma collect_deny_subset: "simple_ruleset rs \<Longrightarrow> collect_deny \<gamma> rs P \<subseteq> P"
+  apply(induction rs arbitrary: P)
+   apply(simp)
+  apply(rename_tac r rs P)
+  apply(case_tac r, rename_tac m a)
+  apply(case_tac a)
+  apply(simp_all add: simple_ruleset_def)
+  apply(fast)
+  apply blast
+  done
+  
+  
+  lemma collect_deny_sound: "simple_ruleset rs \<Longrightarrow> p \<in> collect_deny \<gamma> rs P \<Longrightarrow> approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalDeny"
+  proof(induction rs arbitrary: P)
+  case Nil thus ?case by simp
+  next
+  case (Cons r rs)
+    from Cons obtain m a where r: "r = Rule m a" by (cases r) simp
+    from Cons.prems have simple_rs: "simple_ruleset rs" by (simp add: r simple_ruleset_def)
+    from Cons.prems r have a_cases: "a = Accept \<or> a = Drop" by (simp add: r simple_ruleset_def)
+    show ?case (is ?goal)
+    proof(cases a)
+      case Drop
+        from Drop Cons.IH[where P="{p \<in> P. \<not> matches \<gamma> m Drop p}"] simple_rs have IH:
+          "p \<in> collect_deny \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Drop p} \<Longrightarrow> approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalDeny" by simp
+        from Drop Cons.prems have "(p \<in> P \<and> matches \<gamma> m Drop p) \<or> p \<in> collect_deny \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Drop p}"
+          by(simp add: r)
+        with Drop show ?goal
+        apply -
+        apply(erule disjE)
          apply(simp add: r)
-        apply(simp add: r simple_rs)
-        done
-    qed(insert a_cases, simp_all)
-qed
-
-
-theorem collect_allow_sound_complete: "simple_ruleset rs \<Longrightarrow> {p. p \<in> collect_allow \<gamma> rs UNIV} = {p. approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalAllow}"
-apply(safe)
-using collect_allow_sound[where P=UNIV] apply fast
-using collect_allow_complete[where P=UNIV] by fast
-
-
-
+        apply(simp add: r)
+        using IH by blast
+      next
+      case Accept 
+        from Accept Cons.prems have "p \<in> collect_deny \<gamma> rs {p \<in> P. \<not> matches \<gamma> m Accept p}"
+          by(simp add: r)
+        with Cons.IH simple_rs have "approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalDeny" by simp
+        with Cons show ?goal
+         apply(simp add: r Accept del: approximating_bigstep_fun.simps)
+         apply(simp)
+         using collect_deny_subset[OF simple_rs] by fast
+      qed(insert a_cases, simp_all)
+  qed
+  
+  
+  lemma collect_deny_complete: "simple_ruleset rs \<Longrightarrow> approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalDeny \<Longrightarrow> p \<in> P \<Longrightarrow> p \<in> collect_deny \<gamma> rs P"
+  proof(induction rs arbitrary: P)
+  case Nil thus ?case by simp
+  next
+  case (Cons r rs)
+    from Cons obtain m a where r: "r = Rule m a" by (cases r) simp
+    from Cons.prems have simple_rs: "simple_ruleset rs" by (simp add: r simple_ruleset_def)
+    from Cons.prems r have a_cases: "a = Accept \<or> a = Drop" by (simp add: r simple_ruleset_def)
+    show ?case (is ?goal)
+    proof(cases a)
+      case Accept
+        from Accept Cons.IH simple_rs have IH: "\<forall>P. approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalDeny \<longrightarrow> p \<in> P \<longrightarrow> p \<in> collect_deny \<gamma> rs P" by simp
+        with Accept Cons.prems show ?goal
+          apply(cases "matches \<gamma> m Accept p")
+           apply(simp add: r)
+          apply(simp add: r)
+          done
+      next
+      case Drop
+        with Cons show ?goal 
+          apply(case_tac "matches \<gamma> m Drop p")
+           apply(simp add: r)
+          apply(simp add: r simple_rs)
+          done
+      qed(insert a_cases, simp_all)
+  qed
+  
+  
+  theorem collect_deny_sound_complete: "simple_ruleset rs \<Longrightarrow> {p. p \<in> collect_deny \<gamma> rs UNIV} = {p. approximating_bigstep_fun \<gamma> p rs Undecided = Decision FinalDeny}"
+  apply(safe)
+  using collect_deny_sound[where P=UNIV] apply fast
+  using collect_deny_complete[where P=UNIV] by fast
+  
 
 
 
