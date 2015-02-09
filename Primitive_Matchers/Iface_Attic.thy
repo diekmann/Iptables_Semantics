b1b4788a76b7a8d884807fdf2c2e5b5abc336f85
diekmann <diekmann@net.in.tum.de>
2015-02-09 13:26:15 +0100

    further cleanup

--- Primitive_Matchers/Iface_Attic.thy
+++ Primitive_Matchers/Iface_Attic.thy
@@ -189,5 +189,247 @@ begin
     with a b show ?thesis by blast
    qed
 
+(*
+(* Old stuff below *)
+    (*TODO TODO TODO: a packet has a fixed string as interface, there is no wildcard in it! TODO*)
+    (*TODO: this must be redone! see below*)
+
+fun iface_name_eq :: "string \<Rightarrow> string \<Rightarrow> bool" where
+  "iface_name_eq [] [] \<longleftrightarrow> True" |
+  "iface_name_eq [i1] [] \<longleftrightarrow> (i1 = CHR ''+'')" |
+  "iface_name_eq [] [i2] \<longleftrightarrow> (i2 = CHR ''+'')" |
+  "iface_name_eq [i1] [i2] \<longleftrightarrow> (i1 = CHR ''+'' \<or> i2 = CHR ''+'' \<or> i1 = i2)" |
+  "iface_name_eq (i1#i1s) (i2#i2s) \<longleftrightarrow> (i1 = CHR ''+'' \<and> i1s = [] \<or> i2 = CHR ''+'' \<and> i2s = []) \<or> (i1 = i2 \<and> iface_name_eq i1s i2s)" |
+  "iface_name_eq _ _ \<longleftrightarrow> False"
+
+
+  lemma "iface_name_eq ''lo'' ''lo''" by eval
+  lemma "iface_name_eq ''lo'' ''lo+''" by eval
+  lemma "iface_name_eq ''lo'' ''l+''" by eval
+  lemma "iface_name_eq ''lo'' ''+''" by eval
+  lemma "\<not> iface_name_eq ''lo'' ''lo++''" by eval
+  lemma "\<not> iface_name_eq ''lo'' ''lo+++''" by eval
+  lemma "\<not> iface_name_eq ''lo'' ''lo1+''" by eval
+  lemma "\<not> iface_name_eq ''lo'' ''lo1''" by eval
+  text{*The wildcard interface name*}
+  lemma "iface_name_eq '''' ''+''" by eval
+
+fun iface_name_is_wildcard :: "string \<Rightarrow> bool" where
+  "iface_name_is_wildcard [] \<longleftrightarrow> False" |
+  "iface_name_is_wildcard [s] \<longleftrightarrow> s = CHR ''+''" |
+  "iface_name_is_wildcard (_#ss) \<longleftrightarrow> iface_name_is_wildcard ss"
+lemma iface_name_is_wildcard_alt: "iface_name_is_wildcard eth \<longleftrightarrow> eth \<noteq> [] \<and> hd (rev eth) = CHR ''+''"
+  apply(induction eth rule: iface_name_is_wildcard.induct)
+   apply(simp_all)
+  apply(rename_tac s s' ss)
+  apply(case_tac "rev ss")
+   apply(simp_all)
+  done
+(*lemma iface_name_is_wildcard_cases: "iface_name_is_wildcard eth \<longleftrightarrow> (case rev eth of [] \<Rightarrow> False | s#ss \<Rightarrow> s = CHR ''+'')"
+  apply(induction eth rule: iface_name_is_wildcard.induct)
+   apply(simp_all)
+  apply(rename_tac s s' ss)
+  apply(case_tac "rev ss")
+   apply(simp_all)
+  done*)
+
+definition iface_name_prefix :: "string \<Rightarrow> string" where
+  "iface_name_prefix i \<equiv> (if iface_name_is_wildcard i then butlast i else i)"
+lemma "iface_name_prefix ''eth4'' = ''eth4''" by eval
+lemma "iface_name_prefix ''eth+'' = ''eth''" by eval
+lemma "iface_name_prefix ''eth++'' = ''eth+''" by eval --"the trailing plus is a constant and not a wildcard!"
+lemma "iface_name_prefix '''' = ''''" by eval
+
+lemma "take (length i - 1) i = butlast i" by (metis butlast_conv_take) 
+
+lemma iface_name_eq_alt: "iface_name_eq i1 i2 \<longleftrightarrow> i1 = i2 \<or>
+      iface_name_is_wildcard i1 \<and> take ((length i1) - 1) i1 = take ((length i1) - 1) i2 \<or>
+      iface_name_is_wildcard i2 \<and> take ((length i2) - 1) i2 = take ((length i2) - 1) i1"
+apply(induction i1 i2 rule: iface_name_eq.induct)
+       apply(simp_all)
+  apply(simp_all add: iface_name_is_wildcard_alt take_Cons' split:split_if_asm)
+        apply(safe)
+done
+
+lemma iface_name_eq_refl: "iface_name_eq is is" by(simp add: iface_name_eq_alt)
+lemma iface_name_eq_sym: "iface_name_eq i1 i2 \<Longrightarrow> iface_name_eq i2 i1" by(auto simp add: iface_name_eq_alt)
+lemma iface_name_eq_not_trans: "\<lbrakk>i1 = ''eth0''; i2 = ''eth+''; i3 = ''eth1''\<rbrakk> \<Longrightarrow> 
+    iface_name_eq i1 i2 \<Longrightarrow> iface_name_eq i2 i3 \<Longrightarrow> \<not> iface_name_eq i1 i3" by(simp)
+
+lemma "iface_name_eq (i2 # i2s) [] \<Longrightarrow> i2 = CHR ''+'' \<and> i2s = []" by(auto simp add: iface_name_eq_alt)
+
+
+
+text{*Examples*}
+  lemma "iface_name_eq ''eth+'' ''eth3''" by eval
+  lemma "iface_name_eq ''eth+'' ''e+''" by eval
+  lemma "iface_name_eq ''eth+'' ''eth_tun_foobar''" by eval
+  lemma "iface_name_eq ''eth+'' ''eth_tun+++''" by eval
+  lemma "\<not> iface_name_eq ''eth+'' ''wlan+''" by eval
+  lemma "iface_name_eq ''eth1'' ''eth1''" by eval
+  lemma "\<not> iface_name_eq ''eth1'' ''eth2''" by eval
+  lemma "iface_name_eq ''eth+'' ''eth''" by eval
+  lemma "\<not> iface_name_eq ''a'' ''b+''" by eval
+  lemma "iface_name_eq ''+'' ''''" by eval
+  lemma "iface_name_eq ''++++'' ''++''" by eval
+  lemma "\<not> iface_name_eq '''' ''++''" by eval
+  lemma "iface_name_eq ''+'' ''++''" by eval
+  lemma "\<not> iface_name_eq ''ethA+'' ''ethB+''" by eval
+
+  text{*If the interfaces don't end in a wildcard, then @{const iface_name_eq} is just simple equality*}
+  lemma iface_name_eq_case_nowildcard: "\<lbrakk>\<not> iface_name_is_wildcard i1; \<not> iface_name_is_wildcard i2 \<rbrakk> \<Longrightarrow> iface_name_eq i1 i2 \<longleftrightarrow> i1 = i2"
+    apply(simp add: iface_name_is_wildcard_alt iface_name_eq_alt)
+    by blast
+  text{*If there is exactly one wildcard, both interface strings are equal for the length of the wildcard minus one (called @{const iface_name_prefix}}*}
+  lemma iface_name_eq_case_onewildcard: "\<lbrakk>iface_name_is_wildcard i1; \<not> iface_name_is_wildcard i2 \<rbrakk> \<Longrightarrow> iface_name_eq i1 i2 \<longleftrightarrow> 
+      iface_name_prefix i1 = take (length (iface_name_prefix i1)) i2"
+    apply(simp add: iface_name_eq_alt iface_name_prefix_def butlast_conv_take)
+  by (metis diff_le_self min.commute min_def)
+
+  text{*If both are wildcards, then they are equal in their wildcard prefix*}
+  lemma iface_name_eq_case_twowildcard: "\<lbrakk>iface_name_is_wildcard i1; iface_name_is_wildcard i2 \<rbrakk> \<Longrightarrow> iface_name_eq i1 i2 \<longleftrightarrow> 
+    take (min (length (iface_name_prefix i1)) (length (iface_name_prefix i2))) i1 = take (min (length (iface_name_prefix i1)) (length (iface_name_prefix i2))) i2"
+    apply(simp add: iface_name_eq_alt iface_name_prefix_def)
+    apply(safe)
+      apply (metis min.commute take_take)
+     apply (metis take_take)
+    by (metis min_def)
+  
+    text{*If both are wildcards of equal length, then both iface names are actually equal*}
+    lemma iface_name_eq_case_twowildcardeqlength: 
+      assumes "length i1 = length i2" and "iface_name_is_wildcard i1" and "iface_name_is_wildcard i2"
+      shows "iface_name_eq i1 i2 \<longleftrightarrow> i1 = i2"
+    proof -
+      {
+          fix n have "(min n (n - Suc 0)) = (n - Suc 0)" by linarith
+      } note min_help=this
+      from assms have i1_last: "last i1  = CHR ''+''" and i2_last: "last i2  = CHR ''+''"
+        apply(simp_all add: iface_name_is_wildcard_alt)
+        by (metis hd_rev)+
+      from assms have "i1 \<noteq> []" and "i2 \<noteq> []"
+        by(simp_all add: iface_name_is_wildcard_alt)
+
+      from iface_name_eq_case_twowildcard assms have "iface_name_eq i1 i2 \<longleftrightarrow>
+            take (min (length (iface_name_prefix i1)) (length (iface_name_prefix i2))) i1 = 
+            take (min (length (iface_name_prefix i1)) (length (iface_name_prefix i2))) i2" by simp
+      also have "\<dots> \<longleftrightarrow> take (length i1 - 1) i1 = take (length i1 - 1) i2"
+        by(simp add: assms iface_name_prefix_def min_help)
+      also have "\<dots> \<longleftrightarrow> butlast i1 = butlast i2" using assms(1) butlast_conv_take by metis
+      also have "\<dots> \<longleftrightarrow> butlast i1 @ [last i1] = butlast i2 @ [last i2]"
+        using i1_last i2_last by simp
+      finally show "iface_name_eq i1 i2 \<longleftrightarrow> i1 = i2" using `i1 \<noteq> []` `i2 \<noteq> []` append_butlast_last_id by simp
+    qed
+      
+      
+
+  
+  (*fun iface_name_leq :: "string \<Rightarrow> string \<Rightarrow> bool" where
+    "iface_name_leq [] [] \<longleftrightarrow> True" |
+    "iface_name_leq [i1] [] \<longleftrightarrow> False" |
+    "iface_name_leq [] [i2] \<longleftrightarrow> (i2 = CHR ''+'')" |
+    "iface_name_leq [i1] [i2] \<longleftrightarrow> (i1 = i2 \<or> i2 = CHR ''+'')" |
+    "iface_name_leq (i1#i1s) (i2#i2s) \<longleftrightarrow> (i2 = CHR ''+'' \<and> i2s = []) \<or> (i1 = i2 \<and> iface_name_leq i1s i2s)" |
+    "iface_name_leq _ _ \<longleftrightarrow> False"
+
+
+  lemma "iface_name_leq ''lo'' ''lo''" by eval
+  lemma "iface_name_leq ''lo'' ''lo+''" by eval
+  lemma "iface_name_leq ''lo'' ''l+''" by eval
+  lemma "iface_name_leq ''lo'' ''+''" by eval
+  lemma "\<not> iface_name_leq ''lo+'' ''lo''" by eval
+  lemma "\<not> iface_name_leq ''l+'' ''lo''" by eval
+  lemma "\<not> iface_name_leq ''+'' ''lo''" by eval
+  lemma "\<not> iface_name_leq ''lo'' ''lo++''" by eval
+  lemma "iface_name_leq '''' ''+''" by eval
+  lemma "iface_name_leq ''++'' ''+''" by eval
+  lemma "iface_name_leq ''+'' ''++''" by eval (*NOOO*)
+  lemma "\<not> iface_name_leq ''+'' ''''" by eval
+
+
+  lemma "iface_name_leq i1 i2 \<longleftrightarrow> iface_name_eq i1 i2 \<and> (length (iface_name_prefix i2) \<le> length (iface_name_prefix i1))"
+  apply(induction i1 i2 rule: iface_name_leq.induct)
+         apply(simp_all)
+         nitpick
+    apply(simp_all add: iface_name_is_wildcard_alt take_Cons' split:split_if_asm)
+          apply(safe)
+  done
+  lemma iface_name_leq_alt: "iface_name_leq i1 i2 \<longleftrightarrow> i1 = i2 \<or>
+        iface_name_is_wildcard i2 \<and> take ((length i2) - 1) i2 = take ((length i2) - 1) i1"
+  apply(induction i1 i2 rule: iface_name_leq.induct)
+         apply(simp_all)
+    apply(simp_all add: iface_name_is_wildcard_alt take_Cons' split:split_if_asm)
+          apply(safe)
+  done
+  lemma iface_name_leq_iff_eq: "iface_name_leq i1 i2 \<longleftrightarrow> iface_name_eq i1 i2 \<and> (
+            i1 = i2 \<or> 
+            iface_name_is_wildcard i2 \<and> \<not> iface_name_is_wildcard i1 \<or>
+            iface_name_is_wildcard i1 \<and> iface_name_is_wildcard i2 \<and> take ((length i2) - 1) i2 = take ((length i2) - 1) i1)"
+    apply(induction i1 i2 rule: iface_name_leq.induct)
+           apply(simp_all)
+      apply(simp_all add: take_Cons' iface_name_eq_alt iface_name_is_wildcard_alt)
+      apply(safe)
+      apply(simp_all) (*TODO: indent by 117*)
+    done
+
+
+  lemma iface_name_leq_refl: "iface_name_leq is is" by(simp add: iface_name_leq_alt iface_name_eq_refl)
+  lemma iface_name_leq_sym: "iface_name_leq i1 i2 \<Longrightarrow> iface_name_leq i2 i1 \<Longrightarrow> i1 = i2"
+    nitpick
+    apply(simp add: iface_name_leq_alt iface_name_eq_alt)
+    oops
+  lemma iface_name_leq_not_trans: "\<lbrakk>i1 = ''eth0''; i2 = ''eth+''; i3 = ''eth++''\<rbrakk> \<Longrightarrow> 
+    iface_name_leq i1 i2 \<Longrightarrow> iface_name_leq i2 i3 \<Longrightarrow> \<not> iface_name_leq i1 i3" by(simp)
+
+  lemma "iface_name_leq i1 i2 \<Longrightarrow> (*iface_name_leq i2 i1 \<Longrightarrow>*) iface_name_eq i1 i2" by(simp add: iface_name_leq_iff_eq iface_name_eq_sym)
+  lemma "iface_name_leq i2 i1 \<Longrightarrow> (*iface_name_leq i2 i1 \<Longrightarrow>*) iface_name_eq i1 i2" by(simp add: iface_name_leq_iff_eq iface_name_eq_sym)*)
+
+
+  text{*takes two interface names, returns the most specific one*}
+  definition iface_name_conjuct_merge :: "string \<Rightarrow> string \<Rightarrow> string option" where
+    "iface_name_conjuct_merge i1 i2 \<equiv> (if \<not> iface_name_eq i1 i2 then None else
+      if iface_name_is_wildcard i1 \<and> iface_name_is_wildcard i2 then 
+        (if length i1 \<le> length i2 then Some i2 else Some i1)
+      else if iface_name_is_wildcard i1 then Some i2
+      else Some i1
+      )"
+
+
+  lemma iface_name_conjuct_merge_sym: "iface_name_conjuct_merge i1 i2 = iface_name_conjuct_merge i2 i1"
+    apply(simp add: iface_name_conjuct_merge_def)
+    apply(safe)
+                 apply(simp_all add: iface_name_eq_sym)
+      apply(simp_all add: iface_name_eq_case_nowildcard)
+    apply(simp add: iface_name_eq_case_twowildcardeqlength)
+    done
+
+  lemma "iface_name_conjuct_merge ''lo'' ''lo'' = Some ''lo''" by eval
+  lemma "iface_name_conjuct_merge ''lo'' ''lo+'' = Some ''lo''" by eval
+  lemma "iface_name_conjuct_merge ''lo'' ''l+'' = Some ''lo''" by eval
+  lemma "iface_name_conjuct_merge ''lo'' ''+'' = Some ''lo''" by eval
+  lemma "iface_name_conjuct_merge ''lo'' ''lo++'' = None" by eval
+  lemma "iface_name_conjuct_merge '''' ''+'' = Some ''''" by eval
+  lemma "iface_name_conjuct_merge ''++'' ''+'' = Some ''++''" by eval
+  lemma "iface_name_conjuct_merge ''x'' '''' = None" by eval
+
+
+   lemma "{i. \<exists> X. (iface_name_conjuct_merge i1 i2) = Some X \<and> iface_name_eq X i} = {i. iface_name_eq i1 i} \<inter> {i. iface_name_eq i2 i}"
+    nitpick (*more iface_name_eq on right hand side?*)
+    oops
+
+   lemma "iface_name_eq i1 i \<and> iface_name_eq i2 i \<longleftrightarrow> (case (iface_name_conjuct_merge i1 i2) of 
+      Some X \<Rightarrow> iface_name_eq X i |
+      None \<Rightarrow> False)"
+      oops
+
+
+
+
+(*eth+ and !eth42. Problem!*)
+fun match_iface_name_and :: "string negation_type \<Rightarrow> string negation_type \<Rightarrow> string negation_type option" where
+  "match_iface_name_and (Pos i1) (Pos i2) = (if iface_name_eq i1 i2 then (if length i1 \<ge> length i2 then Some (Pos i1) else Some (Pos i2)) else None)"
+  (*we need the 'shorter' iface. probably we want a pseudo order on the ifaces*)
+    (*An order which is not transitive?*)
+
+*)
 
 end
