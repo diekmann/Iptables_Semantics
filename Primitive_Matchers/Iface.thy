1c9c0cf83ed7c9c0f21ce81d80a76ce4358fa9dc
diekmann <diekmann@net.in.tum.de>
2015-02-09 03:00:15 +0100

    playing with interface sets

--- Primitive_Matchers/Iface.thy
+++ Primitive_Matchers/Iface.thy
@@ -296,6 +296,8 @@ subsection{*Matching*}
     using common_prefix_alt by presburger
   lemma common_prefix_commute: "common_prefix a b \<longleftrightarrow> common_prefix b a"
     unfolding common_prefix_alt by metis
+  lemma common_prefix_append_longer: "length c \<ge> length i \<Longrightarrow> common_prefix i c \<longleftrightarrow> common_prefix i (c@cs)"
+    by(simp add: common_prefix_def min_def)
 
   lemma xxxxx: "length c \<ge> length i \<Longrightarrow> (\<forall>csa. (i::string) @ csa \<noteq> c @ cs) \<longleftrightarrow> \<not> common_prefix i c"
     apply(rule)
@@ -318,7 +320,6 @@ subsection{*Matching*}
 
       have b1: "?B1 = {c@cs | c cs. length c \<ge> length i \<and>  \<not> common_prefix i c}"
         by (metis (full_types) notprefix xxxxx)
-
       have b2: "?B2 = {c@cs | c cs. length c \<le> length i \<and>  \<not> common_prefix i c}"
         apply(rule srule)
         apply(simp add: fun_eq_iff)
@@ -336,7 +337,6 @@ subsection{*Matching*}
         apply(simp add: common_prefix_commute)
         done
 
-
       have "?A \<subseteq> ?B1 \<union> ?B2" 
         apply(subst b1)
         apply(subst b2)
@@ -346,7 +346,6 @@ subsection{*Matching*}
         apply(case_tac "length x \<le> length i")
          apply(auto)[1]
         by (metis nat_le_linear)
-      
       have "?B1 \<union> ?B2 \<subseteq> ?A"
         apply(subst b1)
         apply(subst b2)
@@ -354,11 +353,80 @@ subsection{*Matching*}
       from `?A \<subseteq> ?B1 \<union> ?B2` `?B1 \<union> ?B2 \<subseteq> ?A` show ?thesis by blast
     qed
 
-  lemma "- {i@cs | cs. True} = {c@cs | c cs. \<not> common_prefix i c}"
-    apply(simp add: no_common_prefix)
+  lemma other_char: "a \<noteq> (char_of_nat (Suc (nat_of_char a)))"
+    apply(cases a)
+    apply(simp add: nat_of_char_def char_of_nat_def)
+    sorry
+
+
+  thm Set.full_SetCompr_eq
+  lemma "\<not> (range f) = {u. \<forall>x. u \<noteq> f x}" by blast
+  lemma all_empty_string_False: "(\<forall>cs::string. cs \<noteq> []) \<longleftrightarrow> False" by simp
+
+
+  text{*some @{term common_prefix} sets*}
+  lemma "{c | c. common_prefix i c} \<subseteq> {c@cs| c cs. common_prefix i c}"
     apply(safe)
-     apply(simp_all)
+    apply(simp add: common_prefix_alt)
+    apply (metis append_Nil)
+    done
+  lemma "{c@cs| c cs. length i \<le> length c \<and> common_prefix i c} \<subseteq> {c | c. common_prefix i c}"
+    apply(safe)
+    apply(rule_tac x="c@cs" in exI)
+    apply(simp)
+    apply(subst common_prefix_append_longer[symmetric])
+    apply(simp_all)
+    done
+  lemma "{c | c. common_prefix i c} \<subseteq> {c@cs| c cs. length i \<ge> length c \<and> common_prefix i c}"
+    apply(safe)
+    apply(subst(asm) common_prefix_def)
+    apply(case_tac "length c \<le> length i")
+     apply(simp_all add: min_def split: split_if_asm)
+     apply(rule_tac x=c in exI)
+     apply(simp)
+     apply(simp add: common_prefix_def min_def)
+    apply(rule_tac x="take (length i) c" in exI)
+    apply(simp)
+    apply(simp add: common_prefix_def min_def)
+    by (metis notprefix)
+
+
+  lemma "- {c | c . \<not> common_prefix i c} = {c | c. common_prefix i c}"
+    apply(safe)
+    apply(simp add: common_prefix_alt)
+    done
+  lemma inv_neg_commonprefixset:"- {c@cs| c cs. \<not> common_prefix i c} = {c | c. common_prefix i c}"
+    apply(safe)
+     apply blast
+    apply(simp add: common_prefix_alt)
+    done
+  lemma "- {c@cs | c cs. length c \<le> length i \<and> \<not> common_prefix i c} \<subseteq> {i@cs | cs. True}"
+    apply(safe)
+    apply(subst(asm) common_prefix_def)
+    apply(simp add: min_def)
     oops
+
+  (*TODO: what is this set: {c@cs | c cs. \<not> common_prefix i c} \<union> \<dots>?
+    test: {c | c. length c < length i} \<union> {c@cs | c cs. length c = length i \<and> c \<noteq> i}*)
+  lemma "- {i@cs | cs. True} = {c@cs | c cs. \<not> common_prefix i c} \<union> {c | c. length c < length i}"
+    apply(rule)
+     prefer 2
+     apply(safe)[1]
+     apply(simp add: no_common_prefix)
+     apply(simp add: no_common_prefix)
+    apply(simp)
+    thm Compl_anti_mono[where B="{i @ cs |cs. True}" and A="- {c @ cs |c cs. length c \<le> length i \<and> \<not> common_prefix i c}", simplified]
+    apply(rule Compl_anti_mono[where B="{i @ cs |cs. True}" and A="- ({c@cs | c cs. \<not> common_prefix i c} \<union> {c | c. length c < length i})", simplified])
+    apply(safe)
+    apply(simp)
+    apply(case_tac "(length i) \<le> length x")
+     apply(erule_tac x=x in allE, simp)
+     apply(simp add: common_prefix_alt)
+     (*TODO redoo this proof, this was an old metis run, why does it even prove this?*)
+     apply (metis append_eq_append_conv_if notprefix)
+    apply(simp)
+    done
+    
   
   lemma xxx4: "{s@cs | s cs. length s \<le> length i - 1 \<and> s \<noteq> take (length s) (i::string)} = 
         (\<Union> n \<in> {.. length i - 1}. {s@cs | s cs. length s = n \<and> s \<noteq> take (n) i})" (is "?A = ?B")
