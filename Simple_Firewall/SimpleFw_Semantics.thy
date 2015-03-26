b6e82508137d0ccd6e7a080f1190c259923f9b45
diekmann <diekmann@net.in.tum.de>
2015-03-26 11:30:16 +0100

    discussing simple_match

--- Simple_Firewall/SimpleFw_Semantics.thy
+++ Simple_Firewall/SimpleFw_Semantics.thy
@@ -8,27 +8,58 @@ imports Main "../Bitmagic/IPv4Addr" "../Bitmagic/WordInterval_Lists" "../Semanti
 begin
 
 
-section{*Simple Firewall Syntax (IPv4)*}
-text{*Very TODO*}
+section{*Simple Firewall Syntax (IPv4 only)*}
 (*TODO*)
 
 
   datatype simple_action = Accept | Drop
   
-  (*TODO: Discuss: how did we get rid of the negation types?*)
+  text{*Simple match expressions do not allow negated expressions.
+        However, Most match expressions can still be transformed into simple match expressions.
+        
+        A negated IP address range can be represented as a set of non-negated IP ranges.
+        For example @{text "!8 = {0..7} \<union> {8 .. ipv4max}"}.
+        Using CIDR notation (i.e. the @{text "a.b.c.d/n"} notation), we can represent negated IP
+        ranges as a set of non-negated IP ranges with only fair blowup.
+        Another handy result is that the conjunction of two IP ranges in CIDR notation is 
+        either the smaller of the two ranges or the empty set.
+        An empty IP range cannot be represented.
+        If one wants to represent the empty range, then the complete rule needs to be removed.
+
+        The same holds for layer 4 ports.
+        In addition, there exists an empty port range, e.g. @{text "(1,0)"}.
+        The conjunction of two port ranges is again just one port range.
+        
+        But negation of interfaces is not supported. Since interfaces support a wildcard character,
+        transforming a negated interface would either result in an infeasible blowup or requires
+        knowledge about the existing interfaces (e.g. there only is eth0, eth1, wlan3, and vbox42)
+        An empirical test shows that negated interfaces do not occur in our data sets.
+        Negated interfaces can also be considered bad style: What is !eth0? Everything that is
+        not eth0, experience shows that interfaces may come up randomly, in particular in combination 
+        with virtual machines, so !eth0 might not be the desired match.
+        At the moment, if an negated interface occurs which prevents translation to a simple match,
+        we recommend to abstract the negated interface to unknown and remove it (upper or lower closure
+        rule set) before translating to a simple match.
+        The same discussion holds for negated protocols.
+
+        Noteworthy, simple match expressions are both expressive and support conjunction:
+        @{text "simple-match1 \<and> simple-match2 = simple-match3"}
+        *}
+        (*It took very long to design the simple match such that it can represent everything we need
+        and that you can calculate with it. Disjunction is easy: just have two consecutive rules with the same action.
+        Conjunction was a tough fight! It is needed to translate:
+        common_primitive_match_to_simple_match (MatchAny e1 e2) =
+          simple_match_and (common_primitive_match_to_simple_match e1) (common_primitive_match_to_simple_match e2)
+        This is key to translate common_primitive_match to simple_match
+        *)
   record simple_match =
     iiface :: "iface" --"in-interface" (*TODO: we cannot (and don't want to, c.f. git history) express negated interfaces*)
     oiface :: "iface" --"out-interface"
-
-    (*Negated IP ranges may not occur. Use CIDR split*)
     src :: "(ipv4addr \<times> nat) " --"source IP address"
     dst :: "(ipv4addr \<times> nat) " --"destination"
     proto :: "protocol"
     sports :: "(16 word \<times> 16 word)" --"source-port first:last"
     dports :: "(16 word \<times> 16 word)" --"destination-port first:last"
-    (*ports have no negation type as this can be represented by multiple firewall rules
-      for example: !(3,4) is representable by (1,2) and (4,65535)
-      *)
 
 
   datatype simple_rule = SimpleRule simple_match simple_action
@@ -65,6 +96,18 @@ subsection{*Simple Firewall Semantics*}
     "simple_match_any \<equiv> \<lparr>iiface=IfaceAny, oiface=IfaceAny, src=(0,0), dst=(0,0), proto=ProtoAny, sports=(0,65535), dports=(0,65535) \<rparr>"
 
   lemma simple_match_any: "simple_matches simple_match_any p"
+    apply(simp add: simple_match_any_def ipv4range_set_from_bitmask_0)
+    apply(subgoal_tac "(65535::16 word) = max_word")
+     apply(simp add: match_IfaceAny)
+    apply(simp add: max_word_def)
+    done
+
+
+  text{*we specify only one empty port range*}
+  definition simple_match_none :: "simple_match" where
+    "simple_match_none \<equiv> \<lparr>iiface=IfaceAny, oiface=IfaceAny, src=(1,0), dst=(0,0), proto=ProtoAny, sports=(0,65535), dports=(0,65535) \<rparr>"
+
+  lemma simple_match_none: "simple_matches simple_match_any p"
     apply(simp add: simple_match_any_def ipv4range_set_from_bitmask_0)
     apply(subgoal_tac "(65535::16 word) = max_word")
      apply(simp add: match_IfaceAny)
