69bb4bf93da692ef5c950cd8f22c583770242e75
diekmann <diekmann@net.in.tum.de>
2015-06-21 07:30:59 +0200

    refactored

--- Misc.thy
+++ Misc.thy
@@ -25,42 +25,47 @@ begin
   definition "remdups_rev rs = (rev (remdups (rev rs)))"
   value "remdups_rev [1, 1::int, 1, 2 ,1]"
   
-  private lemma help1: "set rs \<inter> set rs2 = {} \<Longrightarrow> remdups (rs @ rs2) = remdups rs @ remdups rs2"
-  by(induction rs arbitrary: rs2) (simp_all)
-  
-  private lemma help2: "r \<notin> set rs \<Longrightarrow> remdups (rev rs @ [r]) = remdups (rev rs) @ [r]"
-  by (simp add: help1) 
   
   private lemma help3: "remdups (rs @ rs2) = remdups [r\<leftarrow>rs . r \<notin> set rs2] @ remdups rs2"
   by(induction rs arbitrary: rs2) (simp_all)
   
-  private lemma help4: "set rs \<inter> set rs2 \<noteq> {} \<Longrightarrow> remdups_rev (rs @ rs2) = remdups_rev rs @ remdups_rev (filter (\<lambda>r. r \<notin> set rs) rs2)"
-  apply(induction rs arbitrary: rs2)
-   apply(simp_all add: remdups_rev_def)
-  apply(simp add: help3 rev_filter)
-  done
+  private lemma "set rs \<inter> set rs2 \<noteq> {} \<Longrightarrow> remdups_rev (rs @ rs2) = remdups_rev rs @ remdups_rev (filter (\<lambda>r. r \<notin> set rs) rs2)"
+  proof(induction rs arbitrary: rs2)
+  case Cons thus ?case by(simp add: help3 rev_filter remdups_rev_def)
+  qed(simp)
   
-  private lemma help5: "remdups_rev (r#rs) = (if r \<in> set rs then r#remdups_rev (rev (removeAll r (rev rs))) else r#remdups_rev rs)"
-  apply(simp add: remdups_rev_def)
-  apply(safe)
-   prefer 2
-   apply(induction rs)
-    apply(simp_all add: help2)
-   apply (metis append_butlast_last_id append_eq_appendI butlast.simps(2) help2 last.simps last_ConsR list.distinct(2) rev.simps(2) set_ConsD)
-  apply(induction rs)
-   apply(simp_all)
-  apply(simp add: removeAll_filter_not_eq help3)
-  apply(safe)
-    apply(simp_all)
-   apply metis
-  apply metis
-  done
+  private lemma help5: "r \<notin> set rs \<Longrightarrow> remdups_rev (r # rs) = r # remdups_rev rs"
+    unfolding remdups_rev_def
+    proof(induction rs)
+    case (Cons r rs)
+      { fix rs and rs2::"'a list"
+        have "set rs \<inter> set rs2 = {} \<Longrightarrow> remdups (rs @ rs2) = remdups rs @ remdups rs2"
+          by(induction rs arbitrary: rs2) (simp_all)
+      } note h=this
+      { fix r and rs::"'a list"
+        from h[of "rev rs" "[r]"] have "r \<notin> set rs \<Longrightarrow> remdups (rev rs @ [r]) = remdups (rev rs) @ [r]" by simp
+      }
+      with Cons show ?case by fastforce  
+    qed(simp)
+
+
+  private lemma help5': "r \<in> set rs \<Longrightarrow> remdups_rev (r # rs) = r # remdups_rev (rev (removeAll r (rev rs)))"
+    unfolding remdups_rev_def
+    proof(induction rs)
+    case Cons thus ?case
+      apply(simp add: removeAll_filter_not_eq help3)
+      apply(safe)
+        apply(simp_all)
+       apply metis
+      apply metis
+      done
+    qed(simp)
   
   private lemma help6: "rev (removeAll r (rev rs)) = removeAll r rs"
   by (simp add: removeAll_filter_not_eq rev_filter)
   
   lemma remdups_rev_fst: "remdups_rev (r#rs) = (if r \<in> set rs then r#remdups_rev (removeAll r rs) else r#remdups_rev rs)"
-    using help5 help6 by metis
+    using help5 help5' help6 by metis
 
 
   lemma remdups_rev_removeAll: "remdups_rev (removeAll r rs) = removeAll r (remdups_rev rs)"
