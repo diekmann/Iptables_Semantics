7423accaf9a611b2f29c4eff38937bcae2aee4b7
diekmann <diekmann@net.in.tum.de>
2015-06-23 16:17:38 +0200

    refactored

--- Common/Remdups_Rev.thy
+++ Common/Remdups_Rev.thy
@@ -52,39 +52,44 @@ fun remdups_rev_code :: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list" wh
   "remdups_rev_code _ [] = []" |
   "remdups_rev_code ps (r#rs) = (if r \<in> set ps then remdups_rev_code ps rs else r#remdups_rev_code (r#ps) rs)"
 
-lemma  remdups_rev_code_ps_seteq: "set ps1 = set ps2 \<Longrightarrow> remdups_rev_code ps1 rs = remdups_rev_code ps2 rs"
-  apply(induction rs arbitrary: ps1 ps2)
-   apply(simp)
-  apply(subst remdups_rev_code.simps)+
-  apply(case_tac "a \<in> set ps1")
-   apply metis (*simplifier loops*)
-  apply(simp)
-  done
 
-lemma remdups_rev_code_ps_append: "remdups_rev_code (ps1@ps2) rs = remdups_rev_code ps2 (filter (\<lambda>r. r \<notin> set ps1) rs)"
-  proof(induction rs arbitrary: ps1 ps2)
-  case (Cons r rs)
-    have "remdups_rev_code (r # ps1 @ ps2) rs = remdups_rev_code (ps1 @ r # ps2) rs"
-      apply(rule remdups_rev_code_ps_seteq)
-      by simp
-    with Cons.IH have "remdups_rev_code (r # ps1 @ ps2) rs = remdups_rev_code (r#ps2) [r\<leftarrow>rs . r \<notin> set ps1]" by simp
-    from this show ?case by(simp add: Cons)
-  qed(simp add: remdups_rev_def)
-
-lemma remdups_rev_code_ps_fst: "remdups_rev_code (p # ps) rs = remdups_rev_code ps (removeAll p rs)"
-  apply(simp add: remdups_rev_code_ps_append[of "[p]" "ps" rs, simplified] removeAll_filter_not_eq)
-  by metis
-
-lemma remdups_rev_code_removeAll: "remdups_rev_code ps (removeAll p rs) = removeAll p (remdups_rev_code ps rs)"
-  apply(induction rs arbitrary: ps)
-   apply(simp_all)
-  apply(safe)
-   apply(simp_all)
-  apply(simp add: remdups_rev_code_ps_fst removeAll_filter_not_eq)
-  done
-
-lemma [code_unfold]:"remdups_rev rs = remdups_rev_code [] rs"
+lemma remdups_rev_code[code_unfold]: "remdups_rev rs = remdups_rev_code [] rs"
 proof -
+  { fix ps1 ps2 p and rs::"'a list"
+    have "set ps1 = set ps2 \<Longrightarrow> remdups_rev_code ps1 rs = remdups_rev_code ps2 rs"
+      apply(induction rs arbitrary: ps1 ps2)
+       apply(simp)
+      apply(subst remdups_rev_code.simps)+
+      apply(case_tac "a \<in> set ps1")
+       apply metis (*simplifier loops*)
+      apply(simp)
+      done
+  } note remdups_rev_code_ps_seteq=this
+  { fix ps1 ps2 p and rs::"'a list"
+    have "remdups_rev_code (ps1@ps2) rs = remdups_rev_code ps2 (filter (\<lambda>r. r \<notin> set ps1) rs)"
+      proof(induction rs arbitrary: ps1 ps2)
+      case (Cons r rs)
+        have "remdups_rev_code (r # ps1 @ ps2) rs = remdups_rev_code (ps1 @ r # ps2) rs"
+          apply(rule remdups_rev_code_ps_seteq)
+          by simp
+        with Cons.IH have "remdups_rev_code (r # ps1 @ ps2) rs = remdups_rev_code (r#ps2) [r\<leftarrow>rs . r \<notin> set ps1]" by simp
+        from this show ?case by(simp add: Cons)
+      qed(simp add: remdups_rev_def)
+   } note remdups_rev_code_ps_append=this
+  { fix ps p and rs::"'a list"
+    have "remdups_rev_code (p # ps) rs = remdups_rev_code ps (removeAll p rs)"
+      apply(simp add: remdups_rev_code_ps_append[of "[p]" "ps" rs, simplified] removeAll_filter_not_eq)
+      by metis
+  } note remdups_rev_code_ps_fst=this
+  { fix ps p and rs::"'a list"
+    have "remdups_rev_code ps (removeAll p rs) = removeAll p (remdups_rev_code ps rs)"
+      apply(induction rs arbitrary: ps)
+       apply(simp_all)
+      apply(safe)
+       apply(simp_all)
+      apply(simp add: remdups_rev_code_ps_fst removeAll_filter_not_eq)
+      done
+  } note remdups_rev_code_removeAll=this
   {fix ps
     have "\<forall>p \<in> set ps. p \<notin> set rs \<Longrightarrow> remdups_rev rs = remdups_rev_code ps rs"
       apply(induction rs arbitrary: ps)
