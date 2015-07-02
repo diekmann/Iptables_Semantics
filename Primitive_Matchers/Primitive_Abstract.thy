2628c967c837d8a4878d1cecbf9101a9b0da7ccb
diekmann <diekmann@net.in.tum.de>
2015-07-02 14:38:27 +0200

    abstract_primitive_in_doubt_deny

--- Primitive_Matchers/Primitive_Abstract.thy
+++ Primitive_Matchers/Primitive_Abstract.thy
@@ -153,12 +153,10 @@ theorem abstract_primitive_in_doubt_allow:
   proof -
     from simple have "good_ruleset rs" using simple_imp_good_ruleset by fast
     from optimize_matches_simple_ruleset simple simple_imp_good_ruleset have
-      "good_ruleset (optimize_matches (abstract_primitive disc) rs)" by fast
+     good:  "good_ruleset (optimize_matches (abstract_primitive disc) rs)" by fast
     with approximating_semantics_iff_fun_good_ruleset abstract_primitive_help1[OF n simple] `good_ruleset rs` show ?allow
       unfolding \<gamma>_def abstract_def by fast
-    from optimize_matches_simple_ruleset simple simple_imp_good_ruleset have
-      "good_ruleset (optimize_matches (abstract_primitive disc) rs)" by fast
-    with approximating_semantics_iff_fun_good_ruleset abstract_primitive_in_doubt_allow_help2[OF n simple] `good_ruleset rs` \<gamma>_def show ?deny 
+    from good approximating_semantics_iff_fun_good_ruleset abstract_primitive_in_doubt_allow_help2[OF n simple] `good_ruleset rs` \<gamma>_def show ?deny 
       unfolding \<gamma>_def abstract_def by fast
   qed
 
@@ -283,16 +281,22 @@ lemma abstract_primitive_in_doubt_deny_help2: assumes n: "\<forall> m \<in> get_
 qed
 
 
-lemma abstract_primitive_in_doubt_deny:
+theorem abstract_primitive_in_doubt_deny:
   assumes n: "\<forall> m \<in> get_match ` set rs. normalized_nnf_match m" and simple: "simple_ruleset rs"
-  shows   "{p. (common_matcher, in_doubt_deny),p\<turnstile> \<langle>rs, Undecided\<rangle> \<Rightarrow>\<^sub>\<alpha> Decision FinalDeny} \<subseteq>
-           {p. (common_matcher, in_doubt_deny),p\<turnstile> \<langle>optimize_matches (abstract_primitive disc) rs, Undecided\<rangle> \<Rightarrow>\<^sub>\<alpha> Decision FinalDeny}"
+  defines "\<gamma> \<equiv> (common_matcher, in_doubt_deny)" and "abstract disc \<equiv> optimize_matches (abstract_primitive disc)"
+  shows   "{p. \<gamma>,p\<turnstile> \<langle>abstract disc rs, Undecided\<rangle> \<Rightarrow>\<^sub>\<alpha> Decision FinalAllow} \<subseteq> {p. \<gamma>,p\<turnstile> \<langle>rs, Undecided\<rangle> \<Rightarrow>\<^sub>\<alpha> Decision FinalAllow}"
+           (is ?allow)
+  and     "{p. \<gamma>,p\<turnstile> \<langle>rs, Undecided\<rangle> \<Rightarrow>\<^sub>\<alpha> Decision FinalDeny} \<subseteq> {p. \<gamma>,p\<turnstile> \<langle>abstract disc rs, Undecided\<rangle> \<Rightarrow>\<^sub>\<alpha> Decision FinalDeny}"
+           (is ?deny)
   proof -
     let ?\<gamma>="(common_matcher, in_doubt_allow) :: (common_primitive \<Rightarrow> simple_packet \<Rightarrow> ternaryvalue) \<times> (action \<Rightarrow> simple_packet \<Rightarrow> bool)"
     from simple have "good_ruleset rs" using simple_imp_good_ruleset by fast
     from optimize_matches_simple_ruleset simple simple_imp_good_ruleset have
-      "good_ruleset (optimize_matches (abstract_primitive disc) rs)" by fast
-    with approximating_semantics_iff_fun_good_ruleset abstract_primitive_in_doubt_deny_help1[OF n simple] `good_ruleset rs` show ?thesis by fast
+      good: "good_ruleset (optimize_matches (abstract_primitive disc) rs)" by fast
+    with approximating_semantics_iff_fun_good_ruleset abstract_primitive_in_doubt_deny_help1[OF n simple] `good_ruleset rs` show ?deny
+      unfolding \<gamma>_def abstract_def by fast
+    from good approximating_semantics_iff_fun_good_ruleset abstract_primitive_in_doubt_deny_help2[OF n simple] `good_ruleset rs` show ?allow
+      unfolding \<gamma>_def abstract_def by fast
   qed
 
 
