2dd098b277ffcbbb860ebb9323d7baf6b0256fa2
diekmann <diekmann@net.in.tum.de>
2015-08-12 18:19:53 +0200

    added haskell exporter example

---
+++ Examples/Code_haskell.thy
@@ -0,0 +1,18 @@
+theory Code_haskell
+imports "../Primitive_Matchers/Parser"
+  "../Simple_Firewall/SimpleFw_toString"
+  "../Semantics_Ternary/Optimizing"
+begin
+
+export_code Match MatchNot MatchAnd MatchAny
+  Src Dst IIface OIface Prot Src_Ports Dst_Ports CT_State Extra
+  dotteddecimal_toString ipv4addr_toString ipv4_cidr_toString action_toString
+  common_primitive_toString
+  simple_rule_toString
+  unfold_ruleset_INPUT unfold_ruleset_FORWARD unfold_ruleset_OUTPUT map_of_string
+  upper_closure
+  ctstate_assume_new
+  to_simple_firewall
+  in Haskell module_name "Network.IPTables.Generated" file "tmp/"
+
+end
