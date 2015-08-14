9a6cdcf3a16a3a1e3ab102e544a57253590f9473
diekmann <diekmann@net.in.tum.de>
2015-08-14 17:05:49 +0200

    fixed

--- Examples/Parser_Test/Parser_Test.thy
+++ Examples/Parser_Test/Parser_Test.thy
@@ -21,73 +21,78 @@ thm parser_test_firewall_FORWARD_default_policy_def
 value[code] "parser_test_firewall"
 lemma "parser_test_firewall \<equiv>
 [(''DOS~Pro-t_ect'',
-  [Rule (MatchAnd (Match (Prot (Proto TCP))) (Match (Dst_Ports [(0x16, 0x16)]))) action.Accept,
-   Rule (MatchAnd (Match (Prot (Proto TCP)))
-          (MatchAnd (Match (CT_State {CT_New}))
-            (MatchAnd (Match (Dst_Ports [(1, 0xFFFF)]))
-              (Match (Extra ''--tcp-flags FIN,SYN,RST,ACK SYN'')))))
-    action.Accept,
-   Rule (Match (Prot (Proto UDP))) Return,
-   Rule (MatchAnd (Match (Prot (Proto ICMP))) (Match (Extra ''-m icmp --icmp-type 3/4'')))
-    action.Accept,
-   Rule (MatchAnd (Match (Prot (Proto ICMP)))
-          (Match (Extra [CHR ''-'', CHR ''m'', CHR '' '', CHR ''c'', CHR ''o'', CHR ''m'', CHR ''m'',
-                         CHR ''e'', CHR ''n'', CHR ''t'', CHR '' '', CHR ''-'', CHR ''-'', CHR ''c'',
-                         CHR ''o'', CHR ''m'', CHR ''m'', CHR ''e'', CHR ''n'', CHR ''t'', CHR '' '',
-                         Char Nibble2 Nibble2, CHR ''!'', Char Nibble2 Nibble2])))
-    action.Accept,
-   Rule (Match (Prot (Proto ICMP))) action.Accept]),
- (''FORWARD'',
-  [Rule (Match (Extra [CHR ''-'', CHR ''-'', CHR ''l'', CHR ''o'', CHR ''g'', CHR ''-'', CHR ''p'',
-                       CHR ''r'', CHR ''e'', CHR ''f'', CHR ''i'', CHR ''x'', CHR '' '',
-                       Char Nibble2 Nibble2, CHR ''!'', CHR ''#'', CHR ''*'', CHR ''~'', CHR ''%'',
-                       CHR ''&'', CHR ''/'', CHR ''('', CHR '')'', CHR ''='', CHR ''?'',
-                       Char Nibble2 Nibble2, CHR '' '', CHR ''-'', CHR ''-'', CHR ''l'', CHR ''o'',
-                       CHR ''g'', CHR ''-'', CHR ''l'', CHR ''e'', CHR ''v'', CHR ''e'', CHR ''l'',
-                       CHR '' '', CHR ''6'']))
-    Log,
-   Rule (Match (Src (Ip4AddrNetmask (127, 0, 0, 0) 8))) action.Drop,
-   Rule (MatchAnd (Match (IIface (Iface ''wlan0'')))
-          (MatchAnd (Match (Prot (Proto TCP)))
-            (MatchAnd (Match (Extra ''-m tcp ! --tcp-flags FIN,SYN,RST,ACK SYN''))
-              (Match (CT_State {CT_New})))))
-    action.Drop,
-   Rule (Match (Extra ''-m conntrack --ctstate NEW,INVALID'')) action.Drop,
-   Rule (MatchAnd (Match (IIface (Iface ''wlan0'')))
-          (MatchAnd (Match (Prot (Proto ICMP)))
-            (Match (CT_State {CT_Established, CT_New, CT_Related, CT_Untracked}))))
-    action.Accept,
-   Rule (Match (CT_State {CT_New, CT_Untracked})) action.Accept,
-   Rule (Match (CT_State {CT_Established, CT_Related})) action.Accept,
-   Rule (MatchNot (Match (IIface (Iface ''eth+'')))) action.Drop,
-   Rule (MatchAnd (Match (Src (Ip4AddrNetmask (100, 0, 0, 0) 24))) (Match (Prot (Proto TCP))))
-    (Call ''DOS~Pro-t_ect''),
-   Rule (MatchNot (Match (Src (Ip4AddrNetmask (131, 159, 0, 0) 16)))) action.Drop,
-   Rule (MatchAnd (Match (Prot (Proto TCP))) (Match (Src_Ports [(0x50, 0x50), (0x1BB, 0x1BB)]))) Return,
-   Rule (MatchAnd (Match (Prot (Proto TCP))) (Match (Dst_Ports [(0x50, 0x50), (0x1BB, 0x1BB)]))) Return,
-   Rule (MatchAnd (Match (Dst (Ip4AddrNetmask (127, 0, 0, 1) 32)))
-          (MatchAnd (Match (OIface (Iface ''eth1.152'')))
-            (MatchAnd (Match (Prot (Proto UDP)))
-              (Match (Dst_Ports [(0x11D9, 0x11D9), (0x1388, 0xFFFF)])))))
-    action.Accept,
-   Rule (MatchAnd (Match (IIface (Iface ''eth0'')))
-          (MatchAnd (Match (Prot (Proto TCP))) (Match (Dst_Ports [(0x16, 0x16)]))))
-    action.Drop,
-   Rule (MatchAnd (Match (IIface (Iface ''eth0'')))
-          (MatchAnd (Match (Prot (Proto TCP)))
-            (Match (Dst_Ports
-                     [(0x15, 0x15), (0x369, 0x36A), (0x138D, 0x138D), (0x138E, 0x138E), (0x50, 0x50),
-                      (0x224, 0x224), (0x6F, 0x6F), (0x37C, 0x37C), (0x801, 0x801)]))))
-    action.Drop,
-   Rule (Match (Src (Ip4AddrRange (127, 0, 0, 1) (127, 0, 10, 0)))) Return,
-   Rule (MatchNot (Match (Dst (Ip4AddrRange (127, 0, 0, 1) (127, 0, 10, 0))))) Return,
-   Rule MatchAny (Goto ''Terminal'')]),
- (''INPUT'', []), (''OUTPUT'', []),
- (''Terminal'',
-  [Rule (MatchAnd (Match (Dst (Ip4AddrNetmask (127, 0, 0, 1) 32)))
-          (MatchAnd (Match (Prot (Proto UDP))) (Match (Src_Ports [(0x35, 0x35)]))))
-    action.Drop,
-   Rule (Match (Dst (Ip4AddrNetmask (127, 42, 0, 1) 32))) Reject, Rule MatchAny Reject])]" by eval
+   [Rule (MatchAnd (Match (Prot (Proto TCP))) (Match (Dst_Ports [(0x16, 0x16)]))) action.Accept,
+    Rule (MatchAnd (Match (Prot (Proto TCP)))
+           (MatchAnd (Match (CT_State {CT_New}))
+             (MatchAnd (Match (Dst_Ports [(1, 0xFFFF)])) (Match (Extra ''--tcp-flags FIN,SYN,RST,ACK SYN'')))))
+     action.Accept,
+    Rule (Match (Prot (Proto UDP))) Return,
+    Rule (MatchAnd (Match (Prot (Proto ICMP))) (Match (Extra ''-m icmp --icmp-type 3/4''))) action.Accept,
+    Rule (MatchAnd (Match (Prot (Proto ICMP)))
+           (Match (Extra [CHR ''-'', CHR ''m'', CHR '' '', CHR ''c'', CHR ''o'', CHR ''m'', CHR ''m'', CHR ''e'',
+                          CHR ''n'', CHR ''t'', CHR '' '', CHR ''-'', CHR ''-'', CHR ''c'', CHR ''o'', CHR ''m'',
+                          CHR ''m'', CHR ''e'', CHR ''n'', CHR ''t'', CHR '' '', Char Nibble2 Nibble2, CHR ''!'',
+                          Char Nibble2 Nibble2])))
+     action.Accept,
+    Rule (MatchAnd (Match (Prot (Proto ICMP)))
+           (Match (Extra [CHR ''-'', CHR ''m'', CHR '' '', CHR ''c'', CHR ''o'', CHR ''m'', CHR ''m'', CHR ''e'',
+                          CHR ''n'', CHR ''t'', CHR '' '', CHR ''-'', CHR ''-'', CHR ''c'', CHR ''o'', CHR ''m'',
+                          CHR ''m'', CHR ''e'', CHR ''n'', CHR ''t'', CHR '' '', Char Nibble2 Nibble2, CHR ''h'',
+                          CHR ''a'', CHR ''s'', CHR '' '', CHR ''s'', CHR ''p'', CHR ''a'', CHR ''c'', CHR ''e'',
+                          Char Nibble2 Nibble2])))
+     action.Accept,
+    Rule (Match (Prot (Proto ICMP))) action.Accept,
+    Rule (MatchAnd (Match (Extra ''!'')) (Match (Prot (Proto ICMP)))) Empty,
+    Rule (MatchAnd (Match (Extra ''!''))
+           (MatchAnd (Match (Prot (Proto TCP))) (MatchNot (Match (Src (Ip4AddrNetmask (131, 159, 0, 0) 16))))))
+     Empty]),
+  (''FORWARD'',
+   [Rule (Match (Extra [CHR ''-'', CHR ''-'', CHR ''l'', CHR ''o'', CHR ''g'', CHR ''-'', CHR ''p'', CHR ''r'',
+                        CHR ''e'', CHR ''f'', CHR ''i'', CHR ''x'', CHR '' '', Char Nibble2 Nibble2, CHR ''!'',
+                        CHR ''#'', CHR ''*'', CHR ''~'', CHR ''%'', CHR ''&'', CHR ''/'', CHR ''('', CHR '')'',
+                        CHR ''='', CHR ''?'', Char Nibble2 Nibble2, CHR '' '', CHR ''-'', CHR ''-'', CHR ''l'',
+                        CHR ''o'', CHR ''g'', CHR ''-'', CHR ''l'', CHR ''e'', CHR ''v'', CHR ''e'', CHR ''l'',
+                        CHR '' '', CHR ''6'']))
+     Log,
+    Rule (Match (Src (Ip4AddrNetmask (127, 0, 0, 0) 8))) action.Drop,
+    Rule (MatchAnd (Match (IIface (Iface ''wlan0'')))
+           (MatchAnd (Match (Prot (Proto TCP)))
+             (MatchAnd (Match (Extra ''-m tcp ! --tcp-flags FIN,SYN,RST,ACK SYN'')) (Match (CT_State {CT_New})))))
+     action.Drop,
+    Rule (Match (Extra ''-m conntrack --ctstate NEW,INVALID'')) action.Drop,
+    Rule (MatchAnd (Match (IIface (Iface ''wlan0'')))
+           (MatchAnd (Match (Prot (Proto ICMP))) (Match (CT_State {CT_Established, CT_New, CT_Related, CT_Untracked}))))
+     action.Accept,
+    Rule (Match (CT_State {CT_New, CT_Untracked})) action.Accept,
+    Rule (Match (CT_State {CT_Established, CT_Related})) action.Accept,
+    Rule (MatchNot (Match (IIface (Iface ''eth+'')))) action.Drop,
+    Rule (MatchAnd (Match (Src (Ip4AddrNetmask (100, 0, 0, 0) 24))) (Match (Prot (Proto TCP))))
+     (Call ''DOS~Pro-t_ect''),
+    Rule (MatchNot (Match (Src (Ip4AddrNetmask (131, 159, 0, 0) 16)))) action.Drop,
+    Rule (MatchAnd (Match (Prot (Proto TCP))) (Match (Src_Ports [(0x50, 0x50), (0x1BB, 0x1BB)]))) action.Drop,
+    Rule (MatchAnd (Match (Prot (Proto TCP))) (Match (Dst_Ports [(0x50, 0x50), (0x1BB, 0x1BB)]))) action.Drop,
+    Rule (MatchAnd (Match (Dst (Ip4AddrNetmask (127, 0, 0, 1) 32)))
+           (MatchAnd (Match (OIface (Iface ''eth1.152'')))
+             (MatchAnd (Match (Prot (Proto UDP))) (Match (Dst_Ports [(0x11D9, 0x11D9), (0x1388, 0xFFFF)])))))
+     action.Accept,
+    Rule (MatchAnd (Match (IIface (Iface ''eth0'')))
+           (MatchAnd (Match (Prot (Proto TCP))) (Match (Dst_Ports [(0x16, 0x16)]))))
+     action.Drop,
+    Rule (MatchAnd (Match (IIface (Iface ''eth0'')))
+           (MatchAnd (Match (Prot (Proto TCP)))
+             (Match (Dst_Ports
+                      [(0x15, 0x15), (0x369, 0x36A), (0x138D, 0x138D), (0x138E, 0x138E), (0x50, 0x50), (0x224, 0x224),
+                       (0x6F, 0x6F), (0x37C, 0x37C), (0x801, 0x801)]))))
+     action.Drop,
+    Rule (Match (Src (Ip4AddrRange (127, 0, 0, 1) (127, 0, 10, 0)))) Return,
+    Rule (MatchNot (Match (Dst (Ip4AddrRange (127, 0, 0, 1) (127, 0, 10, 0))))) Return,
+    Rule MatchAny (Goto ''Terminal'')]),
+  (''INPUT'', []), (''OUTPUT'', []),
+  (''Terminal'',
+   [Rule (MatchAnd (Match (Dst (Ip4AddrNetmask (127, 0, 0, 1) 32)))
+           (MatchAnd (Match (Prot (Proto UDP))) (Match (Src_Ports [(0x35, 0x35)]))))
+     action.Drop,
+    Rule (Match (Dst (Ip4AddrNetmask (127, 42, 0, 1) 32))) Reject, Rule MatchAny Reject])]" by eval
 
 
 value[code] "map (\<lambda>(c,rs). (c, map (common_primitive_rule_toString) rs)) parser_test_firewall"
