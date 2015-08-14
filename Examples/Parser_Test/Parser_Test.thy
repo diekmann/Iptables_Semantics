264ac408ba193a4cb8ce6d83ced278087eeeb6d5
diekmann <diekmann@net.in.tum.de>
2015-08-14 15:51:46 +0200

    fixed the whitespace error
    but why is parser_test broken?

--- Examples/Parser_Test/Parser_Test.thy
+++ Examples/Parser_Test/Parser_Test.thy
@@ -63,6 +63,7 @@ lemma "parser_test_firewall \<equiv>
    Rule (MatchAnd (Match (Src (Ip4AddrNetmask (100, 0, 0, 0) 24))) (Match (Prot (Proto TCP))))
     (Call ''DOS~Pro-t_ect''),
    Rule (MatchNot (Match (Src (Ip4AddrNetmask (131, 159, 0, 0) 16)))) action.Drop,
+   Rule (MatchAnd (Match (Prot (Proto TCP))) (Match (Src_Ports [(0x50, 0x50), (0x1BB, 0x1BB)]))) Return,
    Rule (MatchAnd (Match (Prot (Proto TCP))) (Match (Dst_Ports [(0x50, 0x50), (0x1BB, 0x1BB)]))) Return,
    Rule (MatchAnd (Match (Dst (Ip4AddrNetmask (127, 0, 0, 1) 32)))
           (MatchAnd (Match (OIface (Iface ''eth1.152'')))
