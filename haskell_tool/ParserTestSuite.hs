b874f80cd8655ce9dba40630a1dc90a20d43969f
diekmann <diekmann@net.in.tum.de>
2016-03-14 00:13:01 +0100

    if you have a fullHD screen you can now view the test file without scrolling left or rigth. such awesome. wow. almost 80char limit. wow.

--- haskell_tool/ParserTestSuite.hs
+++ haskell_tool/ParserTestSuite.hs
@@ -17,11 +17,28 @@ expected_result = "*filter\n\
     \:OUTPUT ACCEPT [70:23175]\n\
     \:Terminal - [0:0]\n\
     \-A DOS~Pro-t_ect `ParsedMatch -p tcp' `ParsedMatch --dpts [22]' `ParsedAction -j ACCEPT'\n\
-    \-A DOS~Pro-t_ect `ParsedMatch -p tcp' `ParsedMatch -m state --state NEW' `ParsedMatch --dpts [1:65535]' `ParsedMatch --tcp-flags [TCP_SYN, TCP_ACK, TCP_FIN, TCP_RST] [TCP_SYN]' `ParsedAction -j ACCEPT'\n\
+    \-A DOS~Pro-t_ect `ParsedMatch -p tcp' `ParsedMatch -m state --state NEW' \
+        \`ParsedMatch --dpts [1:65535]' \
+        \`ParsedMatch --tcp-flags [TCP_SYN, TCP_ACK, TCP_FIN, TCP_RST] [TCP_SYN]' \
+        \`ParsedAction -j ACCEPT'\n\
     \-A DOS~Pro-t_ect `ParsedMatch -p udp' `ParsedAction -j RETURN'\n\
-    \-A DOS~Pro-t_ect `ParsedMatch -p icmp' `ParsedMatch ~~-m~~' `ParsedMatch ~~icmp~~' `ParsedMatch ~~--icmp-type~~' `ParsedMatch ~~3/4~~' `ParsedAction -j ACCEPT'\n\
-    \-A DOS~Pro-t_ect `ParsedMatch -p icmp' `ParsedMatch ~~-m~~' `ParsedMatch ~~comment~~' `ParsedMatch ~~--comment~~' `ParsedMatch ~~\"!\"~~' `ParsedAction -j ACCEPT'\n\
-    \-A DOS~Pro-t_ect `ParsedMatch -p icmp' `ParsedMatch ~~-m~~' `ParsedMatch ~~comment~~' `ParsedMatch ~~--comment~~' `ParsedMatch ~~\"has space\"~~' `ParsedAction -j ACCEPT'\n\
+    \-A DOS~Pro-t_ect `ParsedMatch -p icmp' \
+        \`ParsedMatch ~~-m~~' `ParsedMatch ~~icmp~~' \
+        \`ParsedMatch ~~--icmp-type~~' \
+        \`ParsedMatch ~~3/4~~' \
+        \`ParsedAction -j ACCEPT'\n\
+    \-A DOS~Pro-t_ect `ParsedMatch -p icmp' \
+        \`ParsedMatch ~~-m~~' \
+        \`ParsedMatch ~~comment~~' \
+        \`ParsedMatch ~~--comment~~' \
+        \`ParsedMatch ~~\"!\"~~' \
+        \`ParsedAction -j ACCEPT'\n\
+    \-A DOS~Pro-t_ect `ParsedMatch -p icmp' \
+        \`ParsedMatch ~~-m~~' \
+        \`ParsedMatch ~~comment~~' \
+        \`ParsedMatch ~~--comment~~' \
+        \`ParsedMatch ~~\"has space\"~~' \
+        \`ParsedAction -j ACCEPT'\n\
     \-A DOS~Pro-t_ect `ParsedMatch -p icmp' `ParsedAction -j ACCEPT'\n\
     \-A DOS~Pro-t_ect `ParsedNegatedMatch -p icmp'\n\
     \-A DOS~Pro-t_ect `ParsedNegatedMatch -p tcp' `ParsedNegatedMatch -s 131.159.0.0/16'\n\
@@ -29,11 +46,25 @@ expected_result = "*filter\n\
     \-A DOS~Pro-t_ect `ParsedMatch -i vocb' `ParsedMatch -p udp' `ParsedNegatedMatch --spts [67:68]' `ParsedNegatedMatch --dpts [67:68]' `ParsedAction -j ACCEPT'\n\
     \-A FORWARD `ParsedAction -j LOG' `ParsedMatch ~~--log-prefix~~' `ParsedMatch ~~\"!#*~%&/()=?\"~~' `ParsedMatch ~~--log-level~~' `ParsedMatch ~~6~~'\n\
     \-A FORWARD `ParsedMatch -s 127.0.0.0/8' `ParsedAction -j DROP'\n\
-    \-A FORWARD `ParsedMatch -i wlan0' `ParsedMatch -p tcp' `ParsedNegatedMatch --tcp-flags [TCP_SYN, TCP_ACK, TCP_FIN, TCP_RST] [TCP_SYN]' `ParsedMatch -m state --state NEW' `ParsedAction -j DROP'\n\
-    \-A FORWARD `ParsedMatch -p tcp' `ParsedMatch --tcp-flags [TCP_SYN, TCP_ACK, TCP_FIN, TCP_RST] [TCP_SYN]' `ParsedMatch --dpts [80, 443]' `ParsedAction -j ACCEPT'\n\
-    \-A FORWARD `ParsedMatch -p tcp' `ParsedMatch -m state --state NEW' `ParsedMatch --dpts [1:65535]' `ParsedMatch --tcp-flags [TCP_SYN, TCP_ACK, TCP_FIN, TCP_RST] [TCP_SYN]' `ParsedAction -j ACCEPT'\n\
+    \-A FORWARD `ParsedMatch -i wlan0' \
+        \`ParsedMatch -p tcp' \
+        \`ParsedNegatedMatch --tcp-flags [TCP_SYN, TCP_ACK, TCP_FIN, TCP_RST] [TCP_SYN]' \
+        \`ParsedMatch -m state --state NEW' \
+        \`ParsedAction -j DROP'\n\
+    \-A FORWARD `ParsedMatch -p tcp' \
+        \`ParsedMatch --tcp-flags [TCP_SYN, TCP_ACK, TCP_FIN, TCP_RST] [TCP_SYN]' \
+        \`ParsedMatch --dpts [80, 443]' \
+        \`ParsedAction -j ACCEPT'\n\
+    \-A FORWARD `ParsedMatch -p tcp' \
+        \`ParsedMatch -m state --state NEW' \
+        \`ParsedMatch --dpts [1:65535]' \
+        \`ParsedMatch --tcp-flags [TCP_SYN, TCP_ACK, TCP_FIN, TCP_RST] [TCP_SYN]' \
+        \`ParsedAction -j ACCEPT'\n\
     \-A FORWARD `ParsedMatch -m state --state NEW,INVALID' `ParsedAction -j DROP'\n\
-    \-A FORWARD `ParsedMatch -i wlan0' `ParsedMatch -p icmp' `ParsedMatch -m state --state NEW,ESTABLISHED,RELATED,UNTRACKED' `ParsedAction -j ACCEPT'\n\
+    \-A FORWARD `ParsedMatch -i wlan0' \
+        \`ParsedMatch -p icmp' \
+        \`ParsedMatch -m state --state NEW,ESTABLISHED,RELATED,UNTRACKED' \
+        \`ParsedAction -j ACCEPT'\n\
     \-A FORWARD `ParsedMatch -m state --state NEW,UNTRACKED' `ParsedAction -j ACCEPT'\n\
     \-A FORWARD `ParsedMatch -m state --state ESTABLISHED,RELATED' `ParsedAction -j ACCEPT'\n\
     \-A FORWARD `ParsedNegatedMatch -i eth+' `ParsedAction -j DROP'\n\
@@ -41,9 +72,18 @@ expected_result = "*filter\n\
     \-A FORWARD `ParsedNegatedMatch -s 131.159.0.0/16' `ParsedAction -j DROP'\n\
     \-A FORWARD `ParsedMatch -p tcp' `ParsedMatch --spts [80, 443]' `ParsedAction -j DROP'\n\
     \-A FORWARD `ParsedMatch -p tcp' `ParsedMatch --dpts [80, 443]' `ParsedAction -j DROP'\n\
-    \-A FORWARD `ParsedMatch -d 127.0.0.1/32' `ParsedMatch -o eth1.152' `ParsedMatch -p udp' `ParsedMatch --dpts [4569, 5000:65535]' `ParsedAction -j ACCEPT'\n\
-    \-A FORWARD `ParsedMatch -i eth0' `ParsedMatch -p tcp' `ParsedMatch --dpts [22]' `ParsedAction -j DROP'\n\
-    \-A FORWARD `ParsedMatch -i eth0' `ParsedMatch -p tcp' `ParsedMatch --dpts [21, 873:874, 5005, 5006, 80, 548, 111, 892, 2049]' `ParsedAction -j DROP'\n\
+    \-A FORWARD `ParsedMatch -d 127.0.0.1/32' \
+        \`ParsedMatch -o eth1.152' `ParsedMatch -p udp' \
+        \`ParsedMatch --dpts [4569, 5000:65535]' \
+        \`ParsedAction -j ACCEPT'\n\
+    \-A FORWARD `ParsedMatch -i eth0' \
+        \`ParsedMatch -p tcp' \
+        \`ParsedMatch --dpts [22]' \
+        \`ParsedAction -j DROP'\n\
+    \-A FORWARD `ParsedMatch -i eth0' \
+        \`ParsedMatch -p tcp' \
+        \`ParsedMatch --dpts [21, 873:874, 5005, 5006, 80, 548, 111, 892, 2049]' \
+        \`ParsedAction -j DROP'\n\
     \-A FORWARD `ParsedMatch -s 192.168.0.1' `ParsedAction -j LOGDROP (call)'\n\
     \-A FORWARD `ParsedMatch -m iprange --src-range 127.0.0.1-127.0.10.0' `ParsedAction -j RETURN'\n\
     \-A FORWARD `ParsedNegatedMatch -m iprange --dst-range 127.0.0.1-127.0.10.0' `ParsedAction -j RETURN'\n\
@@ -109,8 +149,9 @@ test_spoofing_certification table chain ipassmtString fileName expected_spoofing
 
 
 
-test_spoofing_SQRL = test_spoofing_certification "raw" "PREROUTING" ipassmt_sqrl_hardcoded "../thy/Examples/SQRL_Shorewall/2015_aug_iptables-save-spoofing-protection" expected_spoofing_result "SQRL Spoofing 2015 failed"
-    where expected_spoofing_result = [("ldit", True)
+test_spoofing_SQRL = test_spoofing_certification "raw" "PREROUTING" ipassmt_sqrl_hardcoded rulesetFile expected_spoofing_result "SQRL Spoofing 2015 failed"
+    where rulesetFile = "../thy/Examples/SQRL_Shorewall/2015_aug_iptables-save-spoofing-protection"
+          expected_spoofing_result = [("ldit", True)
                                      , ("lmd", True)
                                      , ("loben", True)
                                      , ("wg", True)
@@ -185,8 +226,9 @@ test_spoofing_TUM_i8 = test_spoofing_certification "filter" "FORWARD" ipassmt_i8
 
 
 test_spoofing_TUM_Net1 :: IO Progress
-test_spoofing_TUM_Net1 = test_spoofing_TUM_i8 "../thy/Examples/TUM_Net_Firewall/iptables-save-2015-05-15_15-23-41_cheating" expected_spoofing_result "computed_result != expected_spoofing_result (almost all interfaces should have spoofing protection)"
-    where expected_spoofing_result = [  ("eth0", True)
+test_spoofing_TUM_Net1 = test_spoofing_TUM_i8 rulesetFile expected_spoofing_result "computed_result != expected_spoofing_result (almost all interfaces should have spoofing protection)"
+    where rulesetFile = "../thy/Examples/TUM_Net_Firewall/iptables-save-2015-05-15_15-23-41_cheating" 
+          expected_spoofing_result = [  ("eth0", True)
                             , ("foo", False)
                             , ("eth1.96", True)
                             , ("eth1.108", True)
@@ -211,8 +253,9 @@ test_spoofing_TUM_Net1 = test_spoofing_TUM_i8 "../thy/Examples/TUM_Net_Firewall/
                             , ("eth1.1024", True)]
 
 
-test_spoofing_TUM_Net2 = test_spoofing_TUM_i8 "../thy/Examples/TUM_Net_Firewall/iptables-save-2015-05-15_14-14-46_cheating" expected_spoofing_result "computed_result != expected_spoofing_result (ifaces foo, 110, 97, 1024 must fail)"
-    where expected_spoofing_result = [  ("eth0", True)
+test_spoofing_TUM_Net2 = test_spoofing_TUM_i8 rulesetFile expected_spoofing_result "computed_result != expected_spoofing_result (ifaces foo, 110, 97, 1024 must fail)"
+    where rulesetFile = "../thy/Examples/TUM_Net_Firewall/iptables-save-2015-05-15_14-14-46_cheating" 
+          expected_spoofing_result = [  ("eth0", True)
                             , ("foo", False)
                             , ("eth1.96", True)
                             , ("eth1.108", True)
@@ -237,8 +280,9 @@ test_spoofing_TUM_Net2 = test_spoofing_TUM_i8 "../thy/Examples/TUM_Net_Firewall/
                             , ("eth1.1024", False)]
 
 
-test_spoofing_TUM_Net3 = test_spoofing_TUM_i8 "../thy/Examples/TUM_Net_Firewall/iptables-save-2015-05-13_10-53-20_cheating" expected_spoofing_result "computed_result != expected_spoofing_result (only ifaces 96 and eth0 have protection)"
-    where expected_spoofing_result = [  ("eth0", True)
+test_spoofing_TUM_Net3 = test_spoofing_TUM_i8 rulesetFile expected_spoofing_result "computed_result != expected_spoofing_result (only ifaces 96 and eth0 have protection)"
+    where rulesetFile = "../thy/Examples/TUM_Net_Firewall/iptables-save-2015-05-13_10-53-20_cheating"
+          expected_spoofing_result = [  ("eth0", True)
                             , ("foo", False)
                             , ("eth1.96", True)
                             , ("eth1.108", False)
