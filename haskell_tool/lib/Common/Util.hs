45260cac04f83f8fd230854bd3bda7477e69d8d4
Lars Hupel <lars.hupel@mytum.de>
2016-04-12 18:53:10 +0200

    rename service partitions

--- haskell_tool/lib/Common/Util.hs
+++ haskell_tool/lib/Common/Util.hs
@@ -1,6 +1,4 @@
-module Common.Util
-( isParseErrorWindowsNewline
-) where
+module Common.Util where
 
 import qualified Data.List as L
 import qualified Text.Parsec.Error --Windows line ending debug
@@ -11,3 +9,9 @@ isParseErrorWindowsNewline err =
         (Text.Parsec.Error.Expect "\"\\n\"" : Text.Parsec.Error.SysUnExpect "\"\\r\"" : _) -> True
         _ -> False
 
+
+sequences :: [a] -> [[a]]
+sequences xs = map return xs ++ ((:) <$> xs <*> sequences xs)
+
+prettyNames :: [String]
+prettyNames = sequences ['a'..'z']
