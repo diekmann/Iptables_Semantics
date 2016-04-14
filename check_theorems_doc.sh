724e0b789ed33c6ab46b1037409154b0c1ec2e2c
diekmann <diekmann@net.in.tum.de>
2016-04-14 19:10:59 +0200

    draft: script to check for thms in doc

---
+++ check_theorems_doc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+THEOREMS_IN_HS=$(find ./haskell_tool -name '*.hs' | xargs grep -i '\-\-[[:space:]]*Theorem:[[:space:]]' | awk -F ': *' '{ print $3 }')
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+function check_thm {
+	filename="./thy/Documentation.thy"
+	if [ -n "$(grep "$@" $filename)" ]; then
+	    echo -n " [ok]"
+	 else
+	    echo -ne " [${RED}FAIL${NC}]"
+	 fi
+}
+
+#echo "$THEOREMS_IN_HS"
+
+#http://askubuntu.com/questions/344407/how-to-read-complete-line-in-for-loop-with-spaces
+IFS=$'\n'
+for THM in $THEOREMS_IN_HS; do
+    echo -n "theorem $THM"
+    check_thm $THM
+    echo
+done
