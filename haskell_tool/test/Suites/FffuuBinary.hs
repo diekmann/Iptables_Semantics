133ae9e29c44103ffca1b35e091b75515ce0103d
diekmann <diekmann@net.in.tum.de>
2016-04-22 12:58:49 +0200

    working test suite for --help

--- haskell_tool/test/Suites/FffuuBinary.hs
+++ haskell_tool/test/Suites/FffuuBinary.hs
@@ -3,10 +3,15 @@ module Suites.FffuuBinary ( tests ) where
 import Test.Tasty
 import Test.Tasty.Golden as Golden
 import qualified Data.ByteString.Lazy.Char8 as B
+import System.Process (readProcess)
 
+diffCmd = \ref new -> ["/usr/bin/diff", "-u", ref, new]
+fffuuBin = "./dist/build/fffuu/fffuu"
 
-test_fffuu_help = Golden.goldenVsString "run fffuu" "test/Suites/GoldenFiles/help" $ do
-    return $ B.pack "this fails"
+--goldenVsString
+test_fffuu_help = Golden.goldenVsStringDiff "run fffuu" diffCmd "test/Suites/GoldenFiles/help" $ do
+    outp <- readProcess fffuuBin ["--help"] ""
+    return $ B.pack outp
 
 tests = testGroup "fffuu compiled binary output" $
   [ test_fffuu_help
