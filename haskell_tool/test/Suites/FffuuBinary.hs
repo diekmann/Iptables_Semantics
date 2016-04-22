39bf7a716377d20011961fc08fa8e5e6531babc0
diekmann <diekmann@net.in.tum.de>
2016-04-22 13:12:41 +0200

    refactored

--- haskell_tool/test/Suites/FffuuBinary.hs
+++ haskell_tool/test/Suites/FffuuBinary.hs
@@ -1,17 +1,25 @@
 module Suites.FffuuBinary ( tests ) where
 
+import Data.List (intercalate)
 import Test.Tasty
 import Test.Tasty.Golden as Golden
 import qualified Data.ByteString.Lazy.Char8 as B
 import System.Process (readProcess)
 
-diffCmd = \ref new -> ["/usr/bin/diff", "-u", ref, new]
-fffuuBin = "./dist/build/fffuu/fffuu"
 
---goldenVsString
-test_fffuu_help = Golden.goldenVsStringDiff "run fffuu" diffCmd "test/Suites/GoldenFiles/help" $ do
-    outp <- readProcess fffuuBin ["--help"] ""
+
+execFffuu :: TestName
+             -> [String] -- command line argument for fffuu
+             -> FilePath -- path to the golden file (expected output)
+             -> TestTree
+execFffuu name argv goldenFile = Golden.goldenVsStringDiff fancyName diffCmd goldenFile $ do
+    outp <- readProcess fffuuBin argv ""
     return $ B.pack outp
+    where diffCmd = \ref new -> ["/usr/bin/diff", "-u", ref, new]
+          fffuuBin = "./dist/build/fffuu/fffuu"
+          fancyName = name ++ ": " ++ fffuuBin ++ " " ++ intercalate " " argv
+
+test_fffuu_help = execFffuu "help" ["--help"] "test/Suites/GoldenFiles/help"
 
 tests = testGroup "fffuu compiled binary output" $
   [ test_fffuu_help
