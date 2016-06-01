8cb1d9054e98221a9c47170fde2cc0c6110765e2
diekmann <diekmann@net.in.tum.de>
2016-06-01 12:19:13 +0200

    ipv6addr_toString

--- thy/Primitive_Matchers/Common_Primitive_toString.thy
+++ thy/Primitive_Matchers/Common_Primitive_toString.thy
@@ -14,19 +14,46 @@ fun dotteddecimal_toString :: "nat \<times> nat \<times> nat \<times> nat \<Righ
 definition ipv4addr_toString :: "ipv4addr \<Rightarrow> string" where
   "ipv4addr_toString ip = dotteddecimal_toString (dotdecimal_of_ipv4addr ip)"
 
-(* TODO: does not really set the correct amount of ':'
 definition ipv6addr_toString :: "ipv6addr \<Rightarrow> string" where
-  "ipv6addr_toString ip = list_separated_toString '':''
-      (\<lambda>pt. case pt of None \<Rightarrow> '':''
+  "ipv6addr_toString ip = (
+    let partslist = ipv6_preferred_to_compressed (int_to_ipv6preferred ip);
+        (*add empty lists to the beginning and end if omission occurs at start/end
+          to join over ':' properly *)
+        fix_start = (\<lambda>ps. case ps of None#_ \<Rightarrow> None#ps | _ \<Rightarrow> ps);
+        fix_end = (\<lambda>ps. case rev ps of None#_ \<Rightarrow> ps@[None] | _ \<Rightarrow> ps)
+    in list_separated_toString '':''
+      (\<lambda>pt. case pt of None \<Rightarrow> ''''
                     |  Some w \<Rightarrow> hex_string_of_word0 w)
-      (ipv6_preferred_to_compressed (int_to_ipv6preferred ip))"
+      ((fix_end \<circ> fix_start) partslist)
+    )"
 
-value[code] "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0x2001 0xDB8 0x0 0x0 0x8 0x800 0x200C 0x417A))"
-value[code] "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0xFF01 0x0 0x0 0x0 0x0 0x0 0x0 0x101))"
-value[code] "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0 0 0 0 0x8 0x800 0x200C 0x417A))"
-value[code] "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0x2001 0xDB8 0 0 0 0 0 0))"
-value[code] "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0 0 0 0 0 0 0 0))"
-*)
+lemma "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0x2001 0xDB8 0x0 0x0 0x8 0x800 0x200C 0x417A))
+            = ''2001:db8::8:800:200c:417a''" by eval --\<open>a unicast address\<close>
+
+lemma "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0xFF01 0x0 0x0 0x0 0x0 0x0 0x0 0x0101)) =
+            ''ff01::101''" by eval --\<open>a multicast address\<close>
+
+lemma "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0 0 0 0 0x8 0x800 0x200C 0x417A)) =
+             ''::8:800:200c:417a''" by eval
+
+lemma "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0x2001 0xDB8 0 0 0 0 0 0)) =
+             ''2001:db8::''" by eval 
+
+lemma "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0xFF00 0 0 0 0 0 0 0)) =
+             ''ff00::''" by eval --\<open>Multicast\<close>
+
+lemma "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0xFE80 0 0 0 0 0 0 0)) =
+             ''fe80::''" by eval --\<open>Link-Local unicast\<close>
+
+lemma "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0 0 0 0 0 0 0 0)) =
+             ''::''" by eval --\<open>unspecified address\<close>
+
+lemma "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0 0 0 0 0 0 0 1)) =
+             ''::1''" by eval --\<open>loopback address\<close>
+
+lemma "ipv6addr_toString (ipv6preferred_to_int (IPv6AddrPreferred 0x2001 0xdb8 0x0 0x1 0x1 0x1 0x1 0x1)) =
+            ''2001:db8:0:1:1:1:1:1''" by eval --\<open>Section 4.2.2 of RFC5952\<close>
+
 
 
 text\<open>Generic function. Whenever possible, use IPv4 or IPv6 pretty printing!\<close>
