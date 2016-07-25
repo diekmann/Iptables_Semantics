0b62b6eaa972657c90fc04a614ec2245c1920b41
diekmann <diekmann@net.in.tum.de>
2016-07-25 15:02:46 +0200

    tuned

--- thy/Simple_Firewall/Generic_SimpleFw.thy
+++ thy/Simple_Firewall/Generic_SimpleFw.thy
@@ -1,35 +1,7 @@
 section\<open>Generalize Simple Firewall\<close>
 theory Generic_SimpleFw
-imports SimpleFw_Semantics "Common/List_Product_More"
+imports SimpleFw_Semantics "Common/List_Product_More" "Common/Option_Helpers"
 begin
-
-
-subsection\<open>Syntactic Helpers\<close>
-
-definition option2set :: "'a option \<Rightarrow> 'a set" where
-  "option2set n \<equiv> (case n of None \<Rightarrow> {} | Some s \<Rightarrow> {s})"
-
-definition option2list :: "'a option \<Rightarrow> 'a list" where
-  "option2list n \<equiv> (case n of None \<Rightarrow> [] | Some s \<Rightarrow> [s])"
-
-lemma set_option2list[simp]: "set (option2list k) = option2set k"
-  unfolding option2list_def option2set_def by (simp split: option.splits)
-
-lemma option2list_simps[simp]: "option2list (Some x) = [x]" "option2list (None) = []"
-  unfolding option2list_def option.simps by(fact refl)+
-
-lemma option2set_None: "option2set None = {}"
-  by(simp add: option2set_def)
-
-lemma option2list_map: "option2list (map_option f n) = map f (option2list n)"
-  by(simp add: option2list_def split: option.split)
-
-lemma option2set_map: "option2set (map_option f n) = f ` option2set n"
-  by(simp add: option2set_def split: option.split)
-
-
-
-section\<open>Generalized Simple firewall\<close>
 
 text\<open>The semantics of the @{term simple_fw} is quite close to @{const List.find}. 
 The idea of the generalized @{term simple_fw} semantics is that you can have anything as the 
