53c4e6dfd290b1e84d2dc29747244e46ba00ded5
diekmann <diekmann@net.in.tum.de>
2016-08-11 14:47:50 +0200

    contrived example

---
+++ thy/Iptables_Semantics/Examples/Contrived/Contrived_Example.thy
@@ -0,0 +1,70 @@
+section\<open>Contrived Example\<close>
+theory Contrived_Example
+imports 
+  "../../Primitive_Matchers/Parser"
+begin
+
+(*
+  internal network: 131.159.21.0/24
+  DMZ: 131.159.15.240/28
+  DMZ must not connect to internal
+*)
+
+parse_iptables_save example_fw="iptables-save"
+
+thm example_fw_def
+thm example_fw_FORWARD_default_policy_def
+
+
+value "map common_primitive_rule_toString
+                  (upper_closure (packet_assume_new
+                    (unfold_ruleset_FORWARD example_fw_FORWARD_default_policy
+                      (map_of_string_ipv4 example_fw))))"
+
+
+value "map simple_rule_ipv4_toString
+              (to_simple_firewall (upper_closure
+                (optimize_matches abstract_for_simple_firewall
+                  (upper_closure (packet_assume_new
+                    (unfold_ruleset_FORWARD example_fw_FORWARD_default_policy
+                      (map_of_string_ipv4 example_fw)))))))"
+
+
+value "map simple_rule_ipv4_toString
+              (to_simple_firewall_without_interfaces ipassmt_generic_ipv4 (upper_closure
+                (optimize_matches abstract_for_simple_firewall
+                  (upper_closure (packet_assume_new
+                    (unfold_ruleset_FORWARD example_fw_FORWARD_default_policy
+                      (map_of_string_ipv4 example_fw)))))))"
+
+
+
+value[code] "access_matrix_pretty_ipv4 parts_connection_ssh
+              (to_simple_firewall_without_interfaces ipassmt_generic_ipv4 (upper_closure
+                (optimize_matches abstract_for_simple_firewall
+                  (upper_closure (packet_assume_new
+                    (unfold_ruleset_FORWARD example_fw_FORWARD_default_policy
+                      (map_of_string_ipv4 example_fw)))))))"
+
+(*
+a |-> {131.159.21.0 .. 131.159.21.255}
+b |-> {131.159.15.240 .. 131.159.15.255}
+c |-> {127.0.0.0 .. 127.255.255.255}
+d |-> {0.0.0.0 .. 126.255.255.255} \<union> {128.0.0.0 .. 131.159.15.239} \<union> {131.159.16.0 .. 131.159.20.255} \<union> {131.159.22.0 .. 255.255.255.255}
+
+(a,a)
+(a,b)
+(a,c)
+(a,d)
+(b,b)
+(b,c)
+(b,d)
+(c,a)
+(c,b)
+(c,c)
+(c,d)
+(d,b)
+
+*)
+
+end
