e13ffb513ec1d78047d8605a33027f50ad56aa57
diekmann <diekmann@net.in.tum.de>
2016-09-05 12:42:13 +0200

    tuned

--- thy/Iptables_Semantics/Common/Repeat_Stabilize.thy
+++ thy/Iptables_Semantics/Common/Repeat_Stabilize.thy
@@ -6,7 +6,7 @@ begin
 text\<open>Repeating something a number of times\<close>
 
 
-text\<open>repeat the application at most n times (param 1) until it stabilizes\<close>
+text\<open>Iterating a function at most @{term n} times (first parameter) until it stabilizes.\<close>
 fun repeat_stabilize :: "nat \<Rightarrow> ('a \<Rightarrow> 'a) \<Rightarrow> 'a \<Rightarrow> 'a" where
   "repeat_stabilize 0 _ v = v" |
   "repeat_stabilize (Suc n) f v = (let v_new = f v in if v = v_new then v else repeat_stabilize n f v_new)"
@@ -17,7 +17,6 @@ lemma repeat_stabilize_funpow: "repeat_stabilize n f v = (f^^n) v"
     have "f v = v \<Longrightarrow> (f^^n) v = v" by(induction n) simp_all
     with Suc show ?case by(simp add: Let_def funpow_swap1)
   qed(simp)
-
 
 lemma repeat_stabilize_induct: "(P m) \<Longrightarrow> (\<And>m. P m \<Longrightarrow> P (f m)) \<Longrightarrow> P (repeat_stabilize n f m)"
   apply(simp add: repeat_stabilize_funpow)
