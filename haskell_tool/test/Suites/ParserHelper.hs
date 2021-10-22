f3a2842d403f9a8c78477af74ee3b5ee65edc335
Josh Soref <jsoref@users.noreply.github.com>
2021-10-21 22:24:00 -0400

    spelling: identity
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- haskell_tool/test/Suites/ParserHelper.hs
+++ haskell_tool/test/Suites/ParserHelper.hs
@@ -13,7 +13,7 @@ import Test.Tasty.HUnit as HU
 
 
 
-test_parse_show_identiy parser str = HU.testCase ("ParserHelper: parse show identity ("++str++")") $ do
+test_parse_show_identity parser str = HU.testCase ("ParserHelper: parse show identity ("++str++")") $ do
     let result = case runParser eofParser () "<test data>" str
                 of Left err -> do error $ show err
                    Right result -> show result
@@ -36,21 +36,21 @@ test_parse_quotedstring str = HU.testCase ("ParserHelper: quoted string (`"++str
 --TODO negative tests!
 
 tests = testGroup "ParserHelper Unit tests" $
-  [ test_parse_show_identiy Network.IPTables.ParserHelper.ipv4addr "192.168.0.1"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv4cidr "192.168.0.1/24"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv4range "192.168.0.1-192.168.0.255"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv6addr "::"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv6cidr "::/64"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv6range "::-::"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv6addr "2001:db8::8:800:200c:417a"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv6addr "ff01::101"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv6addr "::8:800:200c:417a"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv6addr "2001:db8::"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv6addr "ff00::"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv6addr "fe80::"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv6addr "::"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv6addr "::1"
-  , test_parse_show_identiy Network.IPTables.ParserHelper.ipv6addr "2001:db8::1"
+  [ test_parse_show_identity Network.IPTables.ParserHelper.ipv4addr "192.168.0.1"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv4cidr "192.168.0.1/24"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv4range "192.168.0.1-192.168.0.255"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv6addr "::"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv6cidr "::/64"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv6range "::-::"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv6addr "2001:db8::8:800:200c:417a"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv6addr "ff01::101"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv6addr "::8:800:200c:417a"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv6addr "2001:db8::"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv6addr "ff00::"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv6addr "fe80::"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv6addr "::"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv6addr "::1"
+  , test_parse_show_identity Network.IPTables.ParserHelper.ipv6addr "2001:db8::1"
   , test_parse_quotedstring "\"foo\""
   , test_parse_quotedstring "\"foo with some \\\" escaped quote \""
   , test_parse_quotedstring "\"foo with some escaped escape (\\\\) and proper ending quote \\\\\""
